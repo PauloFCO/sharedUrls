3ecf2f4c9fd00d32076dbec8267cd111356f42a3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-27 13:58:23 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clear-ravens-grow.loca.lt/api",
-    AUTH_BASE_URL: "https://clear-ravens-grow.loca.lt/auth"
+    API_BASE_URL: "https://fair-paths-grow.loca.lt/api",
+    AUTH_BASE_URL: "https://fair-paths-grow.loca.lt/auth"
 }
