f447e66687a684ddf803324e9d2324b85d5949ac
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-27 11:00:08 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://giant-kids-stay.loca.lt/api",
-    AUTH_BASE_URL: "https://giant-kids-stay.loca.lt/auth"
+    API_BASE_URL: "https://early-views-grow.loca.lt/api",
+    AUTH_BASE_URL: "https://early-views-grow.loca.lt/auth"
 }
