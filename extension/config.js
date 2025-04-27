59628200bd2d568eec27e48a029e42974f7bc08b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-27 08:52:15 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://odd-brooms-sniff.loca.lt/api",
-    AUTH_BASE_URL: "https://odd-brooms-sniff.loca.lt/auth"
+    API_BASE_URL: "https://shy-llamas-fix.loca.lt/api",
+    AUTH_BASE_URL: "https://shy-llamas-fix.loca.lt/auth"
 }
