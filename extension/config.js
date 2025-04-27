478cab7b81a991cfde2233ac7b36171e6991bcf6
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-27 08:55:04 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://shy-llamas-fix.loca.lt/api",
-    AUTH_BASE_URL: "https://shy-llamas-fix.loca.lt/auth"
+    API_BASE_URL: "https://loud-lies-shout.loca.lt/api",
+    AUTH_BASE_URL: "https://loud-lies-shout.loca.lt/auth"
 }
