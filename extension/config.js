c7fe8539d832a431cc38132b176bc35f39b5db57
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-27 11:48:55 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://early-views-grow.loca.lt/api",
-    AUTH_BASE_URL: "https://early-views-grow.loca.lt/auth"
+    API_BASE_URL: "https://calm-turkeys-melt.loca.lt/api",
+    AUTH_BASE_URL: "https://calm-turkeys-melt.loca.lt/auth"
 }
