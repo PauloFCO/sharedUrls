65a389034664372d8431955ee5d3836b24a90702
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-27 22:01:14 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://breezy-mice-battle.loca.lt/api",
-    AUTH_BASE_URL: "https://breezy-mice-battle.loca.lt/auth"
+    API_BASE_URL: "https://ready-dingos-wear.loca.lt/api",
+    AUTH_BASE_URL: "https://ready-dingos-wear.loca.lt/auth"
 }
