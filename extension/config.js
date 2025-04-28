e478f5a0520f49e1cbb4e2274409909ca53354cc
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-28 04:03:24 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://tiny-walls-go.loca.lt/api",
-    AUTH_BASE_URL: "https://tiny-walls-go.loca.lt/auth"
+    API_BASE_URL: "https://tired-bikes-relate.loca.lt/api",
+    AUTH_BASE_URL: "https://tired-bikes-relate.loca.lt/auth"
 }
