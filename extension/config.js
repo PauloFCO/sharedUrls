e5fefe7b58541c18c51ec22958fc3bc534b6d026
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-28 12:06:15 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://khaki-signs-teach.loca.lt/api",
-    AUTH_BASE_URL: "https://khaki-signs-teach.loca.lt/auth"
+    API_BASE_URL: "/api",
+    AUTH_BASE_URL: "/auth"
 }
