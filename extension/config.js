878ed2bfc98c2e86c97c9f4be7c6967304840629
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-28 02:02:40 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://honest-cobras-press.loca.lt/api",
-    AUTH_BASE_URL: "https://honest-cobras-press.loca.lt/auth"
+    API_BASE_URL: "https://tiny-walls-go.loca.lt/api",
+    AUTH_BASE_URL: "https://tiny-walls-go.loca.lt/auth"
 }
