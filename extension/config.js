fe6ec6a2c0d140e2931e8ad5c3c5b3281cd784b0
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-28 08:04:49 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://social-grapes-thank.loca.lt/api",
-    AUTH_BASE_URL: "https://social-grapes-thank.loca.lt/auth"
+    API_BASE_URL: "https://giant-kiwis-greet.loca.lt/api",
+    AUTH_BASE_URL: "https://giant-kiwis-greet.loca.lt/auth"
 }
