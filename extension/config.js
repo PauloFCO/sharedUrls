bde11f4a0084ec8fb9b52e5bb5a5b6d6149f8e43
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-28 06:04:07 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://tired-bikes-relate.loca.lt/api",
-    AUTH_BASE_URL: "https://tired-bikes-relate.loca.lt/auth"
+    API_BASE_URL: "https://social-grapes-thank.loca.lt/api",
+    AUTH_BASE_URL: "https://social-grapes-thank.loca.lt/auth"
 }
