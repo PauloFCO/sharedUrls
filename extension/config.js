a67488ea58b88e95d03c4e83d8d3e1468d220746
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-29 23:38:39 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://floppy-planes-accept.loca.lt/api",
-    AUTH_BASE_URL: "https://floppy-planes-accept.loca.lt/auth"
+    API_BASE_URL: "https://curly-files-kick.loca.lt/api",
+    AUTH_BASE_URL: "https://curly-files-kick.loca.lt/auth"
 }
