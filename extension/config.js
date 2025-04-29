2bd991131e0693f0a22f42b0721e3daba7225fe1
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-29 15:35:48 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://silent-adults-reply.loca.lt/api",
-    AUTH_BASE_URL: "https://silent-adults-reply.loca.lt/auth"
+    API_BASE_URL: "https://chubby-terms-make.loca.lt/api",
+    AUTH_BASE_URL: "https://chubby-terms-make.loca.lt/auth"
 }
