0fc111283323a3efac1d09d655b5296f79ea6b0e
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-29 19:37:13 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://mighty-crabs-live.loca.lt/api",
-    AUTH_BASE_URL: "https://mighty-crabs-live.loca.lt/auth"
+    API_BASE_URL: "https://yellow-pillows-live.loca.lt/api",
+    AUTH_BASE_URL: "https://yellow-pillows-live.loca.lt/auth"
 }
