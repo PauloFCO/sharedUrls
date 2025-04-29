a8e56eb7c548df7aa54df41ba2793aa94898f836
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-29 21:37:56 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://yellow-pillows-live.loca.lt/api",
-    AUTH_BASE_URL: "https://yellow-pillows-live.loca.lt/auth"
+    API_BASE_URL: "https://floppy-planes-accept.loca.lt/api",
+    AUTH_BASE_URL: "https://floppy-planes-accept.loca.lt/auth"
 }
