7b49c82d4ae534dda70b93c2e8d212998a8a7558
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-30 21:46:32 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://sad-rivers-tell.loca.lt/api",
-    AUTH_BASE_URL: "https://sad-rivers-tell.loca.lt/auth"
+    API_BASE_URL: "https://lucky-wasps-march.loca.lt/api",
+    AUTH_BASE_URL: "https://lucky-wasps-march.loca.lt/auth"
 }
