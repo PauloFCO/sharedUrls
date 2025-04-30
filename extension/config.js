52111df3f19194b77a120ea527b48907d31088f9
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-30 19:45:50 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://bitter-baths-watch.loca.lt/api",
-    AUTH_BASE_URL: "https://bitter-baths-watch.loca.lt/auth"
+    API_BASE_URL: "https://sad-rivers-tell.loca.lt/api",
+    AUTH_BASE_URL: "https://sad-rivers-tell.loca.lt/auth"
 }
