1d1e68fff373e677f47ad373af47530003b58764
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-30 03:40:06 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://poor-poems-argue.loca.lt/api",
-    AUTH_BASE_URL: "https://poor-poems-argue.loca.lt/auth"
+    API_BASE_URL: "https://clean-planes-burn.loca.lt/api",
+    AUTH_BASE_URL: "https://clean-planes-burn.loca.lt/auth"
 }
