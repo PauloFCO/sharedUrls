4912ffb2c341d0c5c0d01741a00070283767686b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-30 11:42:58 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://crazy-yaks-follow.loca.lt/api",
-    AUTH_BASE_URL: "https://crazy-yaks-follow.loca.lt/auth"
+    API_BASE_URL: "https://clear-eels-decide.loca.lt/api",
+    AUTH_BASE_URL: "https://clear-eels-decide.loca.lt/auth"
 }
