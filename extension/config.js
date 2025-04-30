e9ef3d76ca2a55ae0226cd642354c8586ddf1c48
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-30 23:47:15 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://lucky-wasps-march.loca.lt/api",
-    AUTH_BASE_URL: "https://lucky-wasps-march.loca.lt/auth"
+    API_BASE_URL: "https://cold-pumas-pull.loca.lt/api",
+    AUTH_BASE_URL: "https://cold-pumas-pull.loca.lt/auth"
 }
