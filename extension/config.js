f503b50617e56437e7ad9097eed32776e4bbca58
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-30 09:42:15 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://whole-maps-chew.loca.lt/api",
-    AUTH_BASE_URL: "https://whole-maps-chew.loca.lt/auth"
+    API_BASE_URL: "https://crazy-yaks-follow.loca.lt/api",
+    AUTH_BASE_URL: "https://crazy-yaks-follow.loca.lt/auth"
 }
