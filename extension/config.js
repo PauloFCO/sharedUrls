dd10d291777c05778b149647be2e554eef048530
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-30 07:41:32 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clever-bobcats-lose.loca.lt/api",
-    AUTH_BASE_URL: "https://clever-bobcats-lose.loca.lt/auth"
+    API_BASE_URL: "https://whole-maps-chew.loca.lt/api",
+    AUTH_BASE_URL: "https://whole-maps-chew.loca.lt/auth"
 }
