006c6aea9d38be093b8f976b7c309d6a9bdf2c60
PauloFCO <pauloqueteiglesias@gmail.com>
2025-04-30 01:39:23 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://curly-files-kick.loca.lt/api",
-    AUTH_BASE_URL: "https://curly-files-kick.loca.lt/auth"
+    API_BASE_URL: "https://poor-poems-argue.loca.lt/api",
+    AUTH_BASE_URL: "https://poor-poems-argue.loca.lt/auth"
 }
