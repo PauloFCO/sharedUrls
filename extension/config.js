e0ee31109f00a95cf0b9d5a26ccd7275de4c0628
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-01 17:53:44 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fine-moles-beam.loca.lt/api",
-    AUTH_BASE_URL: "https://fine-moles-beam.loca.lt/auth"
+    API_BASE_URL: "https://deep-actors-wink.loca.lt/api",
+    AUTH_BASE_URL: "https://deep-actors-wink.loca.lt/auth"
 }
