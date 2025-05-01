b86d086c72ce84357361734e6d0a2ece3c6fb249
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-01 09:50:52 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fifty-cobras-repeat.loca.lt/api",
-    AUTH_BASE_URL: "https://fifty-cobras-repeat.loca.lt/auth"
+    API_BASE_URL: "https://common-pets-slide.loca.lt/api",
+    AUTH_BASE_URL: "https://common-pets-slide.loca.lt/auth"
 }
