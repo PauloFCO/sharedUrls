4407a17b3f985164a9adc1d1490e512a55c68644
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-01 21:55:12 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://nine-snails-flow.loca.lt/api",
-    AUTH_BASE_URL: "https://nine-snails-flow.loca.lt/auth"
+    API_BASE_URL: "https://cold-sloths-scream.loca.lt/api",
+    AUTH_BASE_URL: "https://cold-sloths-scream.loca.lt/auth"
 }
