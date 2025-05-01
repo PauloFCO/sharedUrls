03f24f7db9a60214a55603ccb2d2a10d670bd8c1
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-01 13:52:18 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://slick-cameras-mix.loca.lt/api",
-    AUTH_BASE_URL: "https://slick-cameras-mix.loca.lt/auth"
+    API_BASE_URL: "https://wide-planets-attend.loca.lt/api",
+    AUTH_BASE_URL: "https://wide-planets-attend.loca.lt/auth"
 }
