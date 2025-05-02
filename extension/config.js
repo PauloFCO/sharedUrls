153b992a5412a39e1eda3700cb246173ae06fa92
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-02 14:00:55 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://stale-weeks-see.loca.lt/api",
-    AUTH_BASE_URL: "https://stale-weeks-see.loca.lt/auth"
+    API_BASE_URL: "https://busy-boxes-swim.loca.lt/api",
+    AUTH_BASE_URL: "https://busy-boxes-swim.loca.lt/auth"
 }
