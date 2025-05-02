841e1cefb66de3209bd505bc36ebdb6d6fbb0748
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-02 01:56:38 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://spotty-wolves-reply.loca.lt/api",
-    AUTH_BASE_URL: "https://spotty-wolves-reply.loca.lt/auth"
+    API_BASE_URL: "https://brave-deer-jump.loca.lt/api",
+    AUTH_BASE_URL: "https://brave-deer-jump.loca.lt/auth"
 }
