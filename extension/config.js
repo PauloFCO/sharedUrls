6d61940e54ca6bbf2e3b2541c6e669aca817db2a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-02 18:02:21 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fluffy-carrots-poke.loca.lt/api",
-    AUTH_BASE_URL: "https://fluffy-carrots-poke.loca.lt/auth"
+    API_BASE_URL: "https://fuzzy-vans-taste.loca.lt/api",
+    AUTH_BASE_URL: "https://fuzzy-vans-taste.loca.lt/auth"
 }
