139ef703df13c2070bebfeb9a0ef161f93e9346d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-02 20:03:04 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fuzzy-vans-taste.loca.lt/api",
-    AUTH_BASE_URL: "https://fuzzy-vans-taste.loca.lt/auth"
+    API_BASE_URL: "https://cool-poems-doubt.loca.lt/api",
+    AUTH_BASE_URL: "https://cool-poems-doubt.loca.lt/auth"
 }
