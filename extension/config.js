ef956a1d1691dfd83cfcfb265301b9794f07f64c
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-02 03:57:21 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://brave-deer-jump.loca.lt/api",
-    AUTH_BASE_URL: "https://brave-deer-jump.loca.lt/auth"
+    API_BASE_URL: "https://witty-worms-behave.loca.lt/api",
+    AUTH_BASE_URL: "https://witty-worms-behave.loca.lt/auth"
 }
