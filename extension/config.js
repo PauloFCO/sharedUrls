16d455e7b2eebe03d604589c6236dfced127efd0
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-02 07:58:46 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://slow-trams-scream.loca.lt/api",
-    AUTH_BASE_URL: "https://slow-trams-scream.loca.lt/auth"
+    API_BASE_URL: "https://gold-boxes-exist.loca.lt/api",
+    AUTH_BASE_URL: "https://gold-boxes-exist.loca.lt/auth"
 }
