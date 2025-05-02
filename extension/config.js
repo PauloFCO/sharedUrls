45e37f8cbffc14009e08ca5b12b17cf17fc8508e
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-02 09:59:29 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://gold-boxes-exist.loca.lt/api",
-    AUTH_BASE_URL: "https://gold-boxes-exist.loca.lt/auth"
+    API_BASE_URL: "https://open-buckets-shave.loca.lt/api",
+    AUTH_BASE_URL: "https://open-buckets-shave.loca.lt/auth"
 }
