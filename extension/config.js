102e17eb217c9a444cd2e8bc4fd74d1b83ddac70
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-03 04:05:55 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://major-kings-build.loca.lt/api",
-    AUTH_BASE_URL: "https://major-kings-build.loca.lt/auth"
+    API_BASE_URL: "https://vast-weeks-read.loca.lt/api",
+    AUTH_BASE_URL: "https://vast-weeks-read.loca.lt/auth"
 }
