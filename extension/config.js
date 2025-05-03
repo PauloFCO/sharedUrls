761193f8ac408f9b9f9abc76472b98b3888e837a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-03 10:08:04 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://deep-foxes-cry.loca.lt/api",
-    AUTH_BASE_URL: "https://deep-foxes-cry.loca.lt/auth"
+    API_BASE_URL: "https://orange-bottles-own.loca.lt/api",
+    AUTH_BASE_URL: "https://orange-bottles-own.loca.lt/auth"
 }
