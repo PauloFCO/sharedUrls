9544a54f2daecac626802a05cb5e676b426cd641
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-03 22:12:21 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://lazy-files-know.loca.lt/api",
-    AUTH_BASE_URL: "https://lazy-files-know.loca.lt/auth"
+    API_BASE_URL: "https://three-hairs-cut.loca.lt/api",
+    AUTH_BASE_URL: "https://three-hairs-cut.loca.lt/auth"
 }
