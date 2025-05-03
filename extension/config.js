495b44ff742b30085f95dbc910a2d786d4ccd90f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-03 16:10:13 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://legal-garlics-play.loca.lt/api",
-    AUTH_BASE_URL: "https://legal-garlics-play.loca.lt/auth"
+    API_BASE_URL: "https://twelve-cycles-pay.loca.lt/api",
+    AUTH_BASE_URL: "https://twelve-cycles-pay.loca.lt/auth"
 }
