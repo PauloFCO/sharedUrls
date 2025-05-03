17e158b693c03da2f28a9995adf7ffaea31e11f0
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-03 18:10:55 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://twelve-cycles-pay.loca.lt/api",
-    AUTH_BASE_URL: "https://twelve-cycles-pay.loca.lt/auth"
+    API_BASE_URL: "https://poor-showers-camp.loca.lt/api",
+    AUTH_BASE_URL: "https://poor-showers-camp.loca.lt/auth"
 }
