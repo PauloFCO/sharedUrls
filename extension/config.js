2c1a90754f51b20d781ec8432c56437c7b23a333
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-04 16:18:46 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://new-plants-take.loca.lt/api",
-    AUTH_BASE_URL: "https://new-plants-take.loca.lt/auth"
+    API_BASE_URL: "https://cute-pets-work.loca.lt/api",
+    AUTH_BASE_URL: "https://cute-pets-work.loca.lt/auth"
 }
