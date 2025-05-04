c4f4a809b4694889da375d59ae81b9beb1146061
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-04 22:20:55 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://swift-dragons-tap.loca.lt/api",
-    AUTH_BASE_URL: "https://swift-dragons-tap.loca.lt/auth"
+    API_BASE_URL: "https://solid-pugs-raise.loca.lt/api",
+    AUTH_BASE_URL: "https://solid-pugs-raise.loca.lt/auth"
 }
