075ecb42220dac3f4fd138cdb95394ba4fe5916d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-04 04:14:30 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://bumpy-lions-walk.loca.lt/api",
-    AUTH_BASE_URL: "https://bumpy-lions-walk.loca.lt/auth"
+    API_BASE_URL: "https://two-pans-tell.loca.lt/api",
+    AUTH_BASE_URL: "https://two-pans-tell.loca.lt/auth"
 }
