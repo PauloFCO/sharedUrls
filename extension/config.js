54140ae5eeef14589b3f7e2906f5b6aadac84c2f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-04 00:13:04 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://three-hairs-cut.loca.lt/api",
-    AUTH_BASE_URL: "https://three-hairs-cut.loca.lt/auth"
+    API_BASE_URL: "https://two-clouds-watch.loca.lt/api",
+    AUTH_BASE_URL: "https://two-clouds-watch.loca.lt/auth"
 }
