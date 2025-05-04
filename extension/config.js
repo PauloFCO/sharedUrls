1dc46e6d7c332b8b6d6e2229dab62ee54079fdfc
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-04 18:19:29 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://cute-pets-work.loca.lt/api",
-    AUTH_BASE_URL: "https://cute-pets-work.loca.lt/auth"
+    API_BASE_URL: "https://clever-pianos-lose.loca.lt/api",
+    AUTH_BASE_URL: "https://clever-pianos-lose.loca.lt/auth"
 }
