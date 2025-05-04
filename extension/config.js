21ab8c94a2f1dff877db87322bd1e0d0cd209a9d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-04 02:13:47 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://two-clouds-watch.loca.lt/api",
-    AUTH_BASE_URL: "https://two-clouds-watch.loca.lt/auth"
+    API_BASE_URL: "https://bumpy-lions-walk.loca.lt/api",
+    AUTH_BASE_URL: "https://bumpy-lions-walk.loca.lt/auth"
 }
