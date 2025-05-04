0ed06f4fc566579faf1be1cd1148b3d33e8473bc
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-04 08:15:55 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://mean-bushes-work.loca.lt/api",
-    AUTH_BASE_URL: "https://mean-bushes-work.loca.lt/auth"
+    API_BASE_URL: "https://upset-lines-punch.loca.lt/api",
+    AUTH_BASE_URL: "https://upset-lines-punch.loca.lt/auth"
 }
