cc337bb894b60fd44816e6b6b41f76f0f7528cad
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-05 02:22:21 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fruity-chicken-join.loca.lt/api",
-    AUTH_BASE_URL: "https://fruity-chicken-join.loca.lt/auth"
+    API_BASE_URL: "https://funny-taxes-follow.loca.lt/api",
+    AUTH_BASE_URL: "https://funny-taxes-follow.loca.lt/auth"
 }
