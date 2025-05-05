bd40509c4532df0822fdbd28963ee0ea07f9a243
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-05 16:27:21 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://solid-facts-stand.loca.lt/api",
-    AUTH_BASE_URL: "https://solid-facts-stand.loca.lt/auth"
+    API_BASE_URL: "https://tame-shirts-ask.loca.lt/api",
+    AUTH_BASE_URL: "https://tame-shirts-ask.loca.lt/auth"
 }
