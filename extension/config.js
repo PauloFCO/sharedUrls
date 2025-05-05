57d421c337ecdc5ba3143ce4300e886507db237d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-05 20:28:47 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://seven-cobras-drum.loca.lt/api",
-    AUTH_BASE_URL: "https://seven-cobras-drum.loca.lt/auth"
+    API_BASE_URL: "https://two-bananas-cough.loca.lt/api",
+    AUTH_BASE_URL: "https://two-bananas-cough.loca.lt/auth"
 }
