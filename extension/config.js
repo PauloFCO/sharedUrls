8318ad6b2c2d1db8d6af97dfd7019e4e5764274a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-05 00:21:38 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://solid-pugs-raise.loca.lt/api",
-    AUTH_BASE_URL: "https://solid-pugs-raise.loca.lt/auth"
+    API_BASE_URL: "https://fruity-chicken-join.loca.lt/api",
+    AUTH_BASE_URL: "https://fruity-chicken-join.loca.lt/auth"
 }
