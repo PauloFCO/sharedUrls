d363c6016fb976efc5aa0d7e07d3b2571cface94
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-05 06:23:47 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://green-rooms-wonder.loca.lt/api",
-    AUTH_BASE_URL: "https://green-rooms-wonder.loca.lt/auth"
+    API_BASE_URL: "https://green-meals-kneel.loca.lt/api",
+    AUTH_BASE_URL: "https://green-meals-kneel.loca.lt/auth"
 }
