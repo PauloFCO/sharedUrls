8c020be5d16c611e404b2464ed096941b17a3f10
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-06 16:35:56 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://deep-rats-drop.loca.lt/api",
-    AUTH_BASE_URL: "https://deep-rats-drop.loca.lt/auth"
+    API_BASE_URL: "https://large-bats-return.loca.lt/api",
+    AUTH_BASE_URL: "https://large-bats-return.loca.lt/auth"
 }
