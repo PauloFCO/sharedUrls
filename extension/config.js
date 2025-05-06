07ec8c4968d8083a6906a588f7c6caf0cc08ab2e
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-06 08:33:05 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://six-numbers-happen.loca.lt/api",
-    AUTH_BASE_URL: "https://six-numbers-happen.loca.lt/auth"
+    API_BASE_URL: "https://brown-buses-show.loca.lt/api",
+    AUTH_BASE_URL: "https://brown-buses-show.loca.lt/auth"
 }
