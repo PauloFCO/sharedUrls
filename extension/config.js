67c712b1569264bc4abb7ceb73438de621502261
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-06 18:36:39 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://large-bats-return.loca.lt/api",
-    AUTH_BASE_URL: "https://large-bats-return.loca.lt/auth"
+    API_BASE_URL: "https://violet-dots-cough.loca.lt/api",
+    AUTH_BASE_URL: "https://violet-dots-cough.loca.lt/auth"
 }
