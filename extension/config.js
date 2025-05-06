9ac97ca6b257429f331f6b27af10d05d01234592
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-06 04:31:39 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://polite-owls-hammer.loca.lt/api",
-    AUTH_BASE_URL: "https://polite-owls-hammer.loca.lt/auth"
+    API_BASE_URL: "https://eighty-banks-chew.loca.lt/api",
+    AUTH_BASE_URL: "https://eighty-banks-chew.loca.lt/auth"
 }
