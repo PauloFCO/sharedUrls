90d2ac7aecb89e973473dc6f3c823c08e3200338
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-06 20:37:22 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://violet-dots-cough.loca.lt/api",
-    AUTH_BASE_URL: "https://violet-dots-cough.loca.lt/auth"
+    API_BASE_URL: "https://brave-apples-shave.loca.lt/api",
+    AUTH_BASE_URL: "https://brave-apples-shave.loca.lt/auth"
 }
