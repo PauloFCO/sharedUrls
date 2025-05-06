d6162f151802a3cd18f6bc461f7c69b1cc15ca40
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-06 00:30:13 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://short-poems-fail.loca.lt/api",
-    AUTH_BASE_URL: "https://short-poems-fail.loca.lt/auth"
+    API_BASE_URL: "https://few-mangos-play.loca.lt/api",
+    AUTH_BASE_URL: "https://few-mangos-play.loca.lt/auth"
 }
