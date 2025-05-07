87340d137a1c4a5a66ca5b84fc2a210e67153bbb
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-07 16:44:35 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://wild-cobras-exist.loca.lt/api",
-    AUTH_BASE_URL: "https://wild-cobras-exist.loca.lt/auth"
+    API_BASE_URL: "https://deep-doors-bake.loca.lt/api",
+    AUTH_BASE_URL: "https://deep-doors-bake.loca.lt/auth"
 }
