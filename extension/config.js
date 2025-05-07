d92911c1652029fdf01e6567c52ffca9e54ce536
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-07 04:40:14 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://early-points-tease.loca.lt/api",
-    AUTH_BASE_URL: "https://early-points-tease.loca.lt/auth"
+    API_BASE_URL: "https://young-deer-work.loca.lt/api",
+    AUTH_BASE_URL: "https://young-deer-work.loca.lt/auth"
 }
