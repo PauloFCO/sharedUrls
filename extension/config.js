c4229fd88e788fbd6f8ce9d445ea30c240002713
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-07 08:41:40 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://gold-coats-sell.loca.lt/api",
-    AUTH_BASE_URL: "https://gold-coats-sell.loca.lt/auth"
+    API_BASE_URL: "https://small-fans-teach.loca.lt/api",
+    AUTH_BASE_URL: "https://small-fans-teach.loca.lt/auth"
 }
