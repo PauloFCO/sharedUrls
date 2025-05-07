0504afc3eea4fa2b5248b3a6ae29552a1b8bd9be
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-07 20:46:01 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://evil-masks-own.loca.lt/api",
-    AUTH_BASE_URL: "https://evil-masks-own.loca.lt/auth"
+    API_BASE_URL: "https://fruity-bottles-take.loca.lt/api",
+    AUTH_BASE_URL: "https://fruity-bottles-take.loca.lt/auth"
 }
