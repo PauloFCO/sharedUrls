c65283f685eee3534daa7090b2609c393952b93b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-07 14:43:52 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://poor-shrimps-shake.loca.lt/api",
-    AUTH_BASE_URL: "https://poor-shrimps-shake.loca.lt/auth"
+    API_BASE_URL: "https://wild-cobras-exist.loca.lt/api",
+    AUTH_BASE_URL: "https://wild-cobras-exist.loca.lt/auth"
 }
