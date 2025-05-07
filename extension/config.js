42456dfc0c073f7817be5241388a22f0ade51f9c
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-07 18:45:18 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://deep-doors-bake.loca.lt/api",
-    AUTH_BASE_URL: "https://deep-doors-bake.loca.lt/auth"
+    API_BASE_URL: "https://evil-masks-own.loca.lt/api",
+    AUTH_BASE_URL: "https://evil-masks-own.loca.lt/auth"
 }
