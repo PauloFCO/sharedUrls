e41ae012c6e4274da7bf147494e1bb4d90cdc79b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-07 02:39:31 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://tricky-insects-wash.loca.lt/api",
-    AUTH_BASE_URL: "https://tricky-insects-wash.loca.lt/auth"
+    API_BASE_URL: "https://early-points-tease.loca.lt/api",
+    AUTH_BASE_URL: "https://early-points-tease.loca.lt/auth"
 }
