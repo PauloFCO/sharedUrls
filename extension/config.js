5e348e5a417fb98bd5258bc0f59aa855e198c57b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-07 22:46:44 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fruity-bottles-take.loca.lt/api",
-    AUTH_BASE_URL: "https://fruity-bottles-take.loca.lt/auth"
+    API_BASE_URL: "https://lazy-seas-clap.loca.lt/api",
+    AUTH_BASE_URL: "https://lazy-seas-clap.loca.lt/auth"
 }
