d6a77bb05027a1ca73f63c2ad070fe36f8ffe9fd
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-08 20:54:35 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://full-sides-happen.loca.lt/api",
-    AUTH_BASE_URL: "https://full-sides-happen.loca.lt/auth"
+    API_BASE_URL: "https://late-grapes-lie.loca.lt/api",
+    AUTH_BASE_URL: "https://late-grapes-lie.loca.lt/auth"
 }
