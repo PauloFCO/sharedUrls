3cdaa0930f232890a344aa3c6a62e046b98fc4c4
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-08 06:49:35 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://shy-parts-play.loca.lt/api",
-    AUTH_BASE_URL: "https://shy-parts-play.loca.lt/auth"
+    API_BASE_URL: "https://cold-worms-judge.loca.lt/api",
+    AUTH_BASE_URL: "https://cold-worms-judge.loca.lt/auth"
 }
