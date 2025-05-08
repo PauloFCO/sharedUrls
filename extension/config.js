300e98fc8b0a9eb059f9d5f9b15ccaa0c1a2ca0f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-08 12:51:44 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://quiet-toes-cover.loca.lt/api",
-    AUTH_BASE_URL: "https://quiet-toes-cover.loca.lt/auth"
+    API_BASE_URL: "https://fifty-colts-send.loca.lt/api",
+    AUTH_BASE_URL: "https://fifty-colts-send.loca.lt/auth"
 }
