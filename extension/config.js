d41e09d123124ef3aa484048c73878f4f8ed3a7f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-08 04:48:52 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://brave-weeks-wonder.loca.lt/api",
-    AUTH_BASE_URL: "https://brave-weeks-wonder.loca.lt/auth"
+    API_BASE_URL: "https://shy-parts-play.loca.lt/api",
+    AUTH_BASE_URL: "https://shy-parts-play.loca.lt/auth"
 }
