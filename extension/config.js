c9cd97ef61ba3d89e82f51b48ef50fca125ddda7
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-08 10:51:01 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://chubby-files-rhyme.loca.lt/api",
-    AUTH_BASE_URL: "https://chubby-files-rhyme.loca.lt/auth"
+    API_BASE_URL: "https://quiet-toes-cover.loca.lt/api",
+    AUTH_BASE_URL: "https://quiet-toes-cover.loca.lt/auth"
 }
