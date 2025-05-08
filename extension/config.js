c9fa3598e73262e1fbb7da9593ccff52b84318bc
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-08 00:47:26 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://lazy-seas-clap.loca.lt/api",
-    AUTH_BASE_URL: "https://lazy-seas-clap.loca.lt/auth"
+    API_BASE_URL: "https://calm-cows-behave.loca.lt/api",
+    AUTH_BASE_URL: "https://calm-cows-behave.loca.lt/auth"
 }
