e478c4219c6c1e880e885c0b7dca6882c9492fed
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-08 02:48:09 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://calm-cows-behave.loca.lt/api",
-    AUTH_BASE_URL: "https://calm-cows-behave.loca.lt/auth"
+    API_BASE_URL: "https://brave-weeks-wonder.loca.lt/api",
+    AUTH_BASE_URL: "https://brave-weeks-wonder.loca.lt/auth"
 }
