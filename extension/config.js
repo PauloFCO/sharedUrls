7bd9928892f8656855bdb51ff54d8c3c30050341
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-09 13:00:21 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://petite-spoons-call.loca.lt/api",
-    AUTH_BASE_URL: "https://petite-spoons-call.loca.lt/auth"
+    API_BASE_URL: "https://heavy-poets-cheer.loca.lt/api",
+    AUTH_BASE_URL: "https://heavy-poets-cheer.loca.lt/auth"
 }
