a00b131a011f98ea77d23704fe81de5ce0313726
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-09 00:56:01 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://evil-snakes-do.loca.lt/api",
-    AUTH_BASE_URL: "https://evil-snakes-do.loca.lt/auth"
+    API_BASE_URL: "https://violet-vans-rhyme.loca.lt/api",
+    AUTH_BASE_URL: "https://violet-vans-rhyme.loca.lt/auth"
 }
