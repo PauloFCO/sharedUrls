6ceb0b91bf0fc40fbcccfd6e1a5bc947da3b29e2
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-09 06:58:11 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://public-maps-grin.loca.lt/api",
-    AUTH_BASE_URL: "https://public-maps-grin.loca.lt/auth"
+    API_BASE_URL: "https://vast-dots-act.loca.lt/api",
+    AUTH_BASE_URL: "https://vast-dots-act.loca.lt/auth"
 }
