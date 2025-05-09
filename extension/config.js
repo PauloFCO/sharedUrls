0ea838119a0512c3c50b1d64b0e5624928e353bc
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-09 19:02:35 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://upset-readers-push.loca.lt/api",
-    AUTH_BASE_URL: "https://upset-readers-push.loca.lt/auth"
+    API_BASE_URL: "https://busy-boxes-change.loca.lt/api",
+    AUTH_BASE_URL: "https://busy-boxes-change.loca.lt/auth"
 }
