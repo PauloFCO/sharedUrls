97089c09da25f07f74e28d69c2c158a479a810cb
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-09 10:59:38 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://nice-flowers-hear.loca.lt/api",
-    AUTH_BASE_URL: "https://nice-flowers-hear.loca.lt/auth"
+    API_BASE_URL: "https://petite-spoons-call.loca.lt/api",
+    AUTH_BASE_URL: "https://petite-spoons-call.loca.lt/auth"
 }
