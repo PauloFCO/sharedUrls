0504405e5047070778a734b1f05667c90ee3f5bf
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-09 23:04:01 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://soft-stars-bow.loca.lt/api",
-    AUTH_BASE_URL: "https://soft-stars-bow.loca.lt/auth"
+    API_BASE_URL: "https://seven-plants-tease.loca.lt/api",
+    AUTH_BASE_URL: "https://seven-plants-tease.loca.lt/auth"
 }
