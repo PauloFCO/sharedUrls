528b551a7bf1ac1276b89f0c3e0aff9ffa570665
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-09 21:03:18 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://busy-boxes-change.loca.lt/api",
-    AUTH_BASE_URL: "https://busy-boxes-change.loca.lt/auth"
+    API_BASE_URL: "https://soft-stars-bow.loca.lt/api",
+    AUTH_BASE_URL: "https://soft-stars-bow.loca.lt/auth"
 }
