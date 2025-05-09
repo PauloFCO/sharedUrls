48ea11f188a5f835aa36ba20b88f0ebee23da5bb
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-09 02:56:46 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://violet-vans-rhyme.loca.lt/api",
-    AUTH_BASE_URL: "https://violet-vans-rhyme.loca.lt/auth"
+    API_BASE_URL: "https://plenty-loops-build.loca.lt/api",
+    AUTH_BASE_URL: "https://plenty-loops-build.loca.lt/auth"
 }
