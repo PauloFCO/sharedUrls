33a69ffd00b0ac1bed5f668294d3d0eeb6f79373
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-09 04:57:28 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://plenty-loops-build.loca.lt/api",
-    AUTH_BASE_URL: "https://plenty-loops-build.loca.lt/auth"
+    API_BASE_URL: "https://public-maps-grin.loca.lt/api",
+    AUTH_BASE_URL: "https://public-maps-grin.loca.lt/auth"
 }
