b4dd5b67ca40e00857367298aa1445e14183aa22
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-10 09:07:36 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://swift-times-shop.loca.lt/api",
-    AUTH_BASE_URL: "https://swift-times-shop.loca.lt/auth"
+    API_BASE_URL: "https://dull-singers-tell.loca.lt/api",
+    AUTH_BASE_URL: "https://dull-singers-tell.loca.lt/auth"
 }
