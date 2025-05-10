699ac5d8553d5c53fddc5f1ec7c73c63c99b48a1
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-10 19:11:12 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://dull-rockets-take.loca.lt/api",
-    AUTH_BASE_URL: "https://dull-rockets-take.loca.lt/auth"
+    API_BASE_URL: "https://witty-houses-smash.loca.lt/api",
+    AUTH_BASE_URL: "https://witty-houses-smash.loca.lt/auth"
 }
