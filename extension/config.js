ec00e23e650fbe43fcdf9c14f2fc718481454add
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-10 11:08:19 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://dull-singers-tell.loca.lt/api",
-    AUTH_BASE_URL: "https://dull-singers-tell.loca.lt/auth"
+    API_BASE_URL: "https://open-bees-create.loca.lt/api",
+    AUTH_BASE_URL: "https://open-bees-create.loca.lt/auth"
 }
