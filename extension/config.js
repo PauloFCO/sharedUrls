1254d5b2fa4ed9acd1bd868374ba17ec580f6399
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-10 17:10:29 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://lazy-apes-sort.loca.lt/api",
-    AUTH_BASE_URL: "https://lazy-apes-sort.loca.lt/auth"
+    API_BASE_URL: "https://dull-rockets-take.loca.lt/api",
+    AUTH_BASE_URL: "https://dull-rockets-take.loca.lt/auth"
 }
