872d1f3697381170fb5696e25ac859ca88933382
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-10 13:09:03 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://open-bees-create.loca.lt/api",
-    AUTH_BASE_URL: "https://open-bees-create.loca.lt/auth"
+    API_BASE_URL: "https://olive-aliens-wink.loca.lt/api",
+    AUTH_BASE_URL: "https://olive-aliens-wink.loca.lt/auth"
 }
