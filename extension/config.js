ac8a281c20d179f358288a0f2316f3d3fcfc5266
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-11 01:13:21 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://red-weeks-tan.loca.lt/api",
-    AUTH_BASE_URL: "https://red-weeks-tan.loca.lt/auth"
+    API_BASE_URL: "https://tender-women-slide.loca.lt/api",
+    AUTH_BASE_URL: "https://tender-women-slide.loca.lt/auth"
 }
