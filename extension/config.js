54cdafc7b337a1e61c2a67b19b1599ee5120ce82
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-11 13:17:42 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://some-spoons-smell.loca.lt/api",
-    AUTH_BASE_URL: "https://some-spoons-smell.loca.lt/auth"
+    API_BASE_URL: "https://nine-jeans-rescue.loca.lt/api",
+    AUTH_BASE_URL: "https://nine-jeans-rescue.loca.lt/auth"
 }
