bfd3bbcb37e928e845e9ccf9b5573b48b9a9b918
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-11 15:18:25 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://nine-jeans-rescue.loca.lt/api",
-    AUTH_BASE_URL: "https://nine-jeans-rescue.loca.lt/auth"
+    API_BASE_URL: "https://dry-plants-cry.loca.lt/api",
+    AUTH_BASE_URL: "https://dry-plants-cry.loca.lt/auth"
 }
