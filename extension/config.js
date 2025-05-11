b8ebe0fe70b26552b9a1031f546174fef35e5b46
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-11 19:19:50 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://late-years-tie.loca.lt/api",
-    AUTH_BASE_URL: "https://late-years-tie.loca.lt/auth"
+    API_BASE_URL: "https://breezy-games-visit.loca.lt/api",
+    AUTH_BASE_URL: "https://breezy-games-visit.loca.lt/auth"
 }
