0d2e940b0b345b559d7a622149f3164f3d5ac1b8
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-11 07:15:32 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://easy-chicken-swim.loca.lt/api",
-    AUTH_BASE_URL: "https://easy-chicken-swim.loca.lt/auth"
+    API_BASE_URL: "https://chilly-parrots-argue.loca.lt/api",
+    AUTH_BASE_URL: "https://chilly-parrots-argue.loca.lt/auth"
 }
