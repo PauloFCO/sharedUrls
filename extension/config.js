5751712f964c9268ecd06441161f9c13921e87b4
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-11 17:19:08 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://dry-plants-cry.loca.lt/api",
-    AUTH_BASE_URL: "https://dry-plants-cry.loca.lt/auth"
+    API_BASE_URL: "https://late-years-tie.loca.lt/api",
+    AUTH_BASE_URL: "https://late-years-tie.loca.lt/auth"
 }
