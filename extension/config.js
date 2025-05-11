1d92200027d077a6a1111b6e83ab13559d54d7da
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-11 11:16:59 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://ninety-clubs-stop.loca.lt/api",
-    AUTH_BASE_URL: "https://ninety-clubs-stop.loca.lt/auth"
+    API_BASE_URL: "https://some-spoons-smell.loca.lt/api",
+    AUTH_BASE_URL: "https://some-spoons-smell.loca.lt/auth"
 }
