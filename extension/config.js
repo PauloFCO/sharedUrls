33163a01e06098918a28bb6abb2bbd6bb7f2dbcb
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-12 09:24:54 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://two-mugs-shave.loca.lt/api",
-    AUTH_BASE_URL: "https://two-mugs-shave.loca.lt/auth"
+    API_BASE_URL: "https://moody-schools-unite.loca.lt/api",
+    AUTH_BASE_URL: "https://moody-schools-unite.loca.lt/auth"
 }
