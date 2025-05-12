d66982f6d630f1e5bcfefbf888bb05a1d42f18e6
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-12 05:23:29 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clear-berries-sell.loca.lt/api",
-    AUTH_BASE_URL: "https://clear-berries-sell.loca.lt/auth"
+    API_BASE_URL: "https://soft-years-fall.loca.lt/api",
+    AUTH_BASE_URL: "https://soft-years-fall.loca.lt/auth"
 }
