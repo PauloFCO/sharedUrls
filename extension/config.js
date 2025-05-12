d3eb3ad0174f7490fd66ce374a857d824d9fde90
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-12 23:29:58 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://happy-teams-drop.loca.lt/api",
-    AUTH_BASE_URL: "https://happy-teams-drop.loca.lt/auth"
+    API_BASE_URL: "https://fast-memes-attack.loca.lt/api",
+    AUTH_BASE_URL: "https://fast-memes-attack.loca.lt/auth"
 }
