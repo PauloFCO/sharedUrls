a4f06586c995524458b6821d46dc6904c9b4bb82
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-12 15:27:05 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "/api",
-    AUTH_BASE_URL: "/auth"
+    API_BASE_URL: "https://nice-results-press.loca.lt/api",
+    AUTH_BASE_URL: "https://nice-results-press.loca.lt/auth"
 }
