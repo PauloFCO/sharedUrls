33b6eaa0bad71fc3b4c7d14b8918cd153b5624c3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-12 01:22:02 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://cyan-humans-notice.loca.lt/api",
-    AUTH_BASE_URL: "https://cyan-humans-notice.loca.lt/auth"
+    API_BASE_URL: "https://cruel-apes-raise.loca.lt/api",
+    AUTH_BASE_URL: "https://cruel-apes-raise.loca.lt/auth"
 }
