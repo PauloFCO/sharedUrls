aa8fcd1c8cd92e2f52bc5821160db9f20af4a940
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-13 01:30:41 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fast-memes-attack.loca.lt/api",
-    AUTH_BASE_URL: "https://fast-memes-attack.loca.lt/auth"
+    API_BASE_URL: "https://warm-kiwis-make.loca.lt/api",
+    AUTH_BASE_URL: "https://warm-kiwis-make.loca.lt/auth"
 }
