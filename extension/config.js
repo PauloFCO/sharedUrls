c6aa87e0cb0cc85b49e85745b9533336e9ea2ff3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-13 13:34:59 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "/api",
-    AUTH_BASE_URL: "/auth"
+    API_BASE_URL: "https://brown-loops-clean.loca.lt/api",
+    AUTH_BASE_URL: "https://brown-loops-clean.loca.lt/auth"
 }
