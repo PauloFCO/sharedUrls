b6bcfeae37d25b821927baf200bacae683bc8b12
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-13 21:37:50 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://mean-clowns-own.loca.lt/api",
-    AUTH_BASE_URL: "https://mean-clowns-own.loca.lt/auth"
+    API_BASE_URL: "https://eight-mice-attend.loca.lt/api",
+    AUTH_BASE_URL: "https://eight-mice-attend.loca.lt/auth"
 }
