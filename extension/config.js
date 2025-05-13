4f4977a1374ba9846297cba7ea2155dd2566ea94
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-13 05:32:07 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "/api",
-    AUTH_BASE_URL: "/auth"
+    API_BASE_URL: "https://old-loops-shake.loca.lt/api",
+    AUTH_BASE_URL: "https://old-loops-shake.loca.lt/auth"
 }
