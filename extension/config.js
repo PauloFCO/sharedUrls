5316dcfbc1f5cd791500770ce6e83045f15446e7
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-14 01:39:16 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://chubby-crabs-brake.loca.lt/api",
-    AUTH_BASE_URL: "https://chubby-crabs-brake.loca.lt/auth"
+    API_BASE_URL: "https://dirty-eels-eat.loca.lt/api",
+    AUTH_BASE_URL: "https://dirty-eels-eat.loca.lt/auth"
 }
