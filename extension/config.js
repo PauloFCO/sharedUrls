b68e367788d3fa802650a12a2052582e7cdf15e0
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-14 07:41:25 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://breezy-towns-sleep.loca.lt/api",
-    AUTH_BASE_URL: "https://breezy-towns-sleep.loca.lt/auth"
+    API_BASE_URL: "https://itchy-bats-hang.loca.lt/api",
+    AUTH_BASE_URL: "https://itchy-bats-hang.loca.lt/auth"
 }
