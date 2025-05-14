c272209b6ed660247f991cc19c5db3a845110500
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-14 23:47:08 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://short-ideas-agree.loca.lt/api",
-    AUTH_BASE_URL: "https://short-ideas-agree.loca.lt/auth"
+    API_BASE_URL: "https://proud-friends-reply.loca.lt/api",
+    AUTH_BASE_URL: "https://proud-friends-reply.loca.lt/auth"
 }
