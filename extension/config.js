c13b5892b2229748750356ad0e723a1e07087dc2
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-14 09:42:07 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://itchy-bats-hang.loca.lt/api",
-    AUTH_BASE_URL: "https://itchy-bats-hang.loca.lt/auth"
+    API_BASE_URL: "https://shaggy-dolls-brush.loca.lt/api",
+    AUTH_BASE_URL: "https://shaggy-dolls-brush.loca.lt/auth"
 }
