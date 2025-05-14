b7a562abc42d0f405c2bacaaa1c8880dcc0a3ab0
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-14 15:44:16 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://wet-carrots-sleep.loca.lt/api",
-    AUTH_BASE_URL: "https://wet-carrots-sleep.loca.lt/auth"
+    API_BASE_URL: "https://calm-eggs-wash.loca.lt/api",
+    AUTH_BASE_URL: "https://calm-eggs-wash.loca.lt/auth"
 }
