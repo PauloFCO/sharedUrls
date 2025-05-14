5bf305cd1b025205346e31f184c80fe06fb21170
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-14 19:45:42 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://shaggy-paws-bathe.loca.lt/api",
-    AUTH_BASE_URL: "https://shaggy-paws-bathe.loca.lt/auth"
+    API_BASE_URL: "https://lemon-pens-juggle.loca.lt/api",
+    AUTH_BASE_URL: "https://lemon-pens-juggle.loca.lt/auth"
 }
