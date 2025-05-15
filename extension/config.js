9b89102047e99cf23580ce7da885ef12bc058a44
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-15 11:51:25 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://light-cities-scream.loca.lt/api",
-    AUTH_BASE_URL: "https://light-cities-scream.loca.lt/auth"
+    API_BASE_URL: "https://loose-ducks-kick.loca.lt/api",
+    AUTH_BASE_URL: "https://loose-ducks-kick.loca.lt/auth"
 }
