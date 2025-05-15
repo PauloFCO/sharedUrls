34f24a9a1ffd29b0b03cae2aa4b73eeb475e92ff
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-15 19:54:17 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://public-eagles-kiss.loca.lt/api",
-    AUTH_BASE_URL: "https://public-eagles-kiss.loca.lt/auth"
+    API_BASE_URL: "https://five-chicken-wash.loca.lt/api",
+    AUTH_BASE_URL: "https://five-chicken-wash.loca.lt/auth"
 }
