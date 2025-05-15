3f024218c39475b44bc8f4393af3db0e9a515d39
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-15 23:55:43 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://small-keys-hammer.loca.lt/api",
-    AUTH_BASE_URL: "https://small-keys-hammer.loca.lt/auth"
+    API_BASE_URL: "https://crazy-moose-marry.loca.lt/api",
+    AUTH_BASE_URL: "https://crazy-moose-marry.loca.lt/auth"
 }
