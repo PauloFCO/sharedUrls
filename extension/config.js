922f5dbdc38d6e6712e1d652d0de7c34c4eae6d8
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-15 15:52:51 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://cute-hoops-prove.loca.lt/api",
-    AUTH_BASE_URL: "https://cute-hoops-prove.loca.lt/auth"
+    API_BASE_URL: "https://salty-cows-bathe.loca.lt/api",
+    AUTH_BASE_URL: "https://salty-cows-bathe.loca.lt/auth"
 }
