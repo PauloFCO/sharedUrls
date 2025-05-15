55f18e9559bd5a55b9ef8d38aaf34422dd6cdf51
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-15 13:52:08 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://loose-ducks-kick.loca.lt/api",
-    AUTH_BASE_URL: "https://loose-ducks-kick.loca.lt/auth"
+    API_BASE_URL: "https://cute-hoops-prove.loca.lt/api",
+    AUTH_BASE_URL: "https://cute-hoops-prove.loca.lt/auth"
 }
