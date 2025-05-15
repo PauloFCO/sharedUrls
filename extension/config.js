4bed0ffe55c88335fe7b6b363217eab05aaf794e
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-15 01:47:51 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://proud-friends-reply.loca.lt/api",
-    AUTH_BASE_URL: "https://proud-friends-reply.loca.lt/auth"
+    API_BASE_URL: "https://metal-parents-shake.loca.lt/api",
+    AUTH_BASE_URL: "https://metal-parents-shake.loca.lt/auth"
 }
