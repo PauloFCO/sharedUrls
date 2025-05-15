50c88ae4797ea4725a20dd68687e37656c2e6835
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-15 03:48:34 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://metal-parents-shake.loca.lt/api",
-    AUTH_BASE_URL: "https://metal-parents-shake.loca.lt/auth"
+    API_BASE_URL: "https://dark-poems-lead.loca.lt/api",
+    AUTH_BASE_URL: "https://dark-poems-lead.loca.lt/auth"
 }
