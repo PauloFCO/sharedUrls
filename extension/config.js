00c509d022e598049dae7fa016427c6b61710640
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-15 07:49:59 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://khaki-baboons-jump.loca.lt/api",
-    AUTH_BASE_URL: "https://khaki-baboons-jump.loca.lt/auth"
+    API_BASE_URL: "https://whole-snails-hug.loca.lt/api",
+    AUTH_BASE_URL: "https://whole-snails-hug.loca.lt/auth"
 }
