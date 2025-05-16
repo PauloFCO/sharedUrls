1793b6a65dbafe4127e05b1b088bffaf0a21240d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-16 05:57:52 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clean-tables-mix.loca.lt/api",
-    AUTH_BASE_URL: "https://clean-tables-mix.loca.lt/auth"
+    API_BASE_URL: "https://red-fans-eat.loca.lt/api",
+    AUTH_BASE_URL: "https://red-fans-eat.loca.lt/auth"
 }
