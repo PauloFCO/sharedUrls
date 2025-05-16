556572a187b96e8e29dcf0e017f18611407617fa
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-16 07:58:35 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://red-fans-eat.loca.lt/api",
-    AUTH_BASE_URL: "https://red-fans-eat.loca.lt/auth"
+    API_BASE_URL: "https://whole-friends-stick.loca.lt/api",
+    AUTH_BASE_URL: "https://whole-friends-stick.loca.lt/auth"
 }
