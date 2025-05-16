76864fd9a71e5d63630765884266dec37ad956fa
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-16 09:59:18 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://whole-friends-stick.loca.lt/api",
-    AUTH_BASE_URL: "https://whole-friends-stick.loca.lt/auth"
+    API_BASE_URL: "https://funny-bats-knock.loca.lt/api",
+    AUTH_BASE_URL: "https://funny-bats-knock.loca.lt/auth"
 }
