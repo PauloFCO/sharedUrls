9a0cd27e55f9681581005c60e63f074155ac2ead
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-16 01:56:26 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://crazy-moose-marry.loca.lt/api",
-    AUTH_BASE_URL: "https://crazy-moose-marry.loca.lt/auth"
+    API_BASE_URL: "https://huge-birds-rhyme.loca.lt/api",
+    AUTH_BASE_URL: "https://huge-birds-rhyme.loca.lt/auth"
 }
