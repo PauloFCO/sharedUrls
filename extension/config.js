1d96dc39d2bc6d853b3d8cd35656c0f36ec213c4
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-17 08:07:10 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clean-tips-strive.loca.lt/api",
-    AUTH_BASE_URL: "https://clean-tips-strive.loca.lt/auth"
+    API_BASE_URL: "https://kind-ghosts-allow.loca.lt/api",
+    AUTH_BASE_URL: "https://kind-ghosts-allow.loca.lt/auth"
 }
