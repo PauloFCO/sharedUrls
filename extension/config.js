4436044cbac3626cad1bae4e9c8b6cac3ca16c10
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-17 22:12:16 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://quick-moons-jog.loca.lt/api",
-    AUTH_BASE_URL: "https://quick-moons-jog.loca.lt/auth"
+    API_BASE_URL: "https://huge-sides-read.loca.lt/api",
+    AUTH_BASE_URL: "https://huge-sides-read.loca.lt/auth"
 }
