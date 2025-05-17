7bba1a097aa6aacf23333a6e0476951494ec3fc3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-17 06:06:27 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://four-clouds-cover.loca.lt/api",
-    AUTH_BASE_URL: "https://four-clouds-cover.loca.lt/auth"
+    API_BASE_URL: "https://clean-tips-strive.loca.lt/api",
+    AUTH_BASE_URL: "https://clean-tips-strive.loca.lt/auth"
 }
