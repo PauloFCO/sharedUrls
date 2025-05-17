f742a804179dc3ce67e5e9d760a6ce6df6a19a4b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-17 04:05:44 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fifty-peaches-sit.loca.lt/api",
-    AUTH_BASE_URL: "https://fifty-peaches-sit.loca.lt/auth"
+    API_BASE_URL: "https://four-clouds-cover.loca.lt/api",
+    AUTH_BASE_URL: "https://four-clouds-cover.loca.lt/auth"
 }
