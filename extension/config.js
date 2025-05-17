2b188eb57e3f75c0c6520dbad794ae12ee5c3702
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-17 00:04:18 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://pretty-mails-go.loca.lt/api",
-    AUTH_BASE_URL: "https://pretty-mails-go.loca.lt/auth"
+    API_BASE_URL: "https://petite-moose-win.loca.lt/api",
+    AUTH_BASE_URL: "https://petite-moose-win.loca.lt/auth"
 }
