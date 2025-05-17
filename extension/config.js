b894dad42ed55afe2c82e213c41f3fe840087828
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-17 16:10:01 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://brown-walls-sink.loca.lt/api",
-    AUTH_BASE_URL: "https://brown-walls-sink.loca.lt/auth"
+    API_BASE_URL: "https://orange-zoos-wait.loca.lt/api",
+    AUTH_BASE_URL: "https://orange-zoos-wait.loca.lt/auth"
 }
