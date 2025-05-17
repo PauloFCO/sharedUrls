75ff5ffbc3165fe1dcafd35eaafcb5ccc72c189b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-17 14:09:18 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://khaki-jobs-hear.loca.lt/api",
-    AUTH_BASE_URL: "https://khaki-jobs-hear.loca.lt/auth"
+    API_BASE_URL: "https://brown-walls-sink.loca.lt/api",
+    AUTH_BASE_URL: "https://brown-walls-sink.loca.lt/auth"
 }
