911f6edb61095af5a5d54b3d71450cf3693150a5
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-17 02:05:01 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://petite-moose-win.loca.lt/api",
-    AUTH_BASE_URL: "https://petite-moose-win.loca.lt/auth"
+    API_BASE_URL: "https://fifty-peaches-sit.loca.lt/api",
+    AUTH_BASE_URL: "https://fifty-peaches-sit.loca.lt/auth"
 }
