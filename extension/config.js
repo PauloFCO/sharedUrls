d78eca79b6fe7b31eccc516fe38a014f5170306d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-18 02:13:41 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://loose-teams-try.loca.lt/api",
-    AUTH_BASE_URL: "https://loose-teams-try.loca.lt/auth"
+    API_BASE_URL: "https://loose-bikes-tickle.loca.lt/api",
+    AUTH_BASE_URL: "https://loose-bikes-tickle.loca.lt/auth"
 }
