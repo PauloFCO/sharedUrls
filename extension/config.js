a6901ccefb1399e079a64fbe4fc4800501569834
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-18 16:18:52 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://nine-meals-read.loca.lt/api",
-    AUTH_BASE_URL: "https://nine-meals-read.loca.lt/auth"
+    API_BASE_URL: "https://true-brooms-strive.loca.lt/api",
+    AUTH_BASE_URL: "https://true-brooms-strive.loca.lt/auth"
 }
