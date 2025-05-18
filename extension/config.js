a8afb2f1d237679f25e660c26581fa709e236d06
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-18 22:21:01 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://gold-boats-leave.loca.lt/api",
-    AUTH_BASE_URL: "https://gold-boats-leave.loca.lt/auth"
+    API_BASE_URL: "https://clever-webs-begin.loca.lt/api",
+    AUTH_BASE_URL: "https://clever-webs-begin.loca.lt/auth"
 }
