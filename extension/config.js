1bbc6d244a09f1a371b5462f2a641e00d5f1725f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-18 06:15:12 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://hungry-colts-exist.loca.lt/api",
-    AUTH_BASE_URL: "https://hungry-colts-exist.loca.lt/auth"
+    API_BASE_URL: "https://empty-beans-say.loca.lt/api",
+    AUTH_BASE_URL: "https://empty-beans-say.loca.lt/auth"
 }
