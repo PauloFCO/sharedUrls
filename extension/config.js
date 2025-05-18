a7faaa2d3269151b7b18f1941945f870b5684dd3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-18 14:18:09 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://stupid-adults-hang.loca.lt/api",
-    AUTH_BASE_URL: "https://stupid-adults-hang.loca.lt/auth"
+    API_BASE_URL: "https://nine-meals-read.loca.lt/api",
+    AUTH_BASE_URL: "https://nine-meals-read.loca.lt/auth"
 }
