7cee4dad96347918a83d3eb349318bea86b9c349
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-18 00:12:59 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://huge-sides-read.loca.lt/api",
-    AUTH_BASE_URL: "https://huge-sides-read.loca.lt/auth"
+    API_BASE_URL: "https://loose-teams-try.loca.lt/api",
+    AUTH_BASE_URL: "https://loose-teams-try.loca.lt/auth"
 }
