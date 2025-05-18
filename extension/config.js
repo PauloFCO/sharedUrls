06185e522b2b7209398ae9f10272111ff512cc12
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-18 20:20:18 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://ninety-garlics-switch.loca.lt/api",
-    AUTH_BASE_URL: "https://ninety-garlics-switch.loca.lt/auth"
+    API_BASE_URL: "https://gold-boats-leave.loca.lt/api",
+    AUTH_BASE_URL: "https://gold-boats-leave.loca.lt/auth"
 }
