ac2538669c8a381b05e13e137c39863e34356b1d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-19 16:27:28 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://long-beds-travel.loca.lt/api",
-    AUTH_BASE_URL: "https://long-beds-travel.loca.lt/auth"
+    API_BASE_URL: "https://cute-geckos-nail.loca.lt/api",
+    AUTH_BASE_URL: "https://cute-geckos-nail.loca.lt/auth"
 }
