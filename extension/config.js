fd5bbed2ebd60c94688d34de53e0a532ef0a48e6
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-19 08:24:37 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://blue-teeth-argue.loca.lt/api",
-    AUTH_BASE_URL: "https://blue-teeth-argue.loca.lt/auth"
+    API_BASE_URL: "https://busy-cooks-march.loca.lt/api",
+    AUTH_BASE_URL: "https://busy-cooks-march.loca.lt/auth"
 }
