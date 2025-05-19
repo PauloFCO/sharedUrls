84dc64d01b25be279dadb215d64cb3eb0407c28f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-19 12:26:03 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://big-beans-lead.loca.lt/api",
-    AUTH_BASE_URL: "https://big-beans-lead.loca.lt/auth"
+    API_BASE_URL: "https://cold-items-buy.loca.lt/api",
+    AUTH_BASE_URL: "https://cold-items-buy.loca.lt/auth"
 }
