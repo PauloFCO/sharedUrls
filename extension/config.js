87dcf6f101f15d1ae901bd10c89a60315637d4b3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-19 06:23:54 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://mean-donkeys-stick.loca.lt/api",
-    AUTH_BASE_URL: "https://mean-donkeys-stick.loca.lt/auth"
+    API_BASE_URL: "https://blue-teeth-argue.loca.lt/api",
+    AUTH_BASE_URL: "https://blue-teeth-argue.loca.lt/auth"
 }
