a677b05c9e6a22db12cf1b6539f72825796f3d21
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-19 18:28:11 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://cute-geckos-nail.loca.lt/api",
-    AUTH_BASE_URL: "https://cute-geckos-nail.loca.lt/auth"
+    API_BASE_URL: "https://breezy-insects-enjoy.loca.lt/api",
+    AUTH_BASE_URL: "https://breezy-insects-enjoy.loca.lt/auth"
 }
