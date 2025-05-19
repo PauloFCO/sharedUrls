a5fbe2932c54ad0df048038144615217622ac6f0
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-19 14:26:45 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://cold-items-buy.loca.lt/api",
-    AUTH_BASE_URL: "https://cold-items-buy.loca.lt/auth"
+    API_BASE_URL: "https://long-beds-travel.loca.lt/api",
+    AUTH_BASE_URL: "https://long-beds-travel.loca.lt/auth"
 }
