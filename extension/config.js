5724543ad96f0e4f0c8d9756a4259bc029aba862
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-19 02:22:27 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://moody-candies-raise.loca.lt/api",
-    AUTH_BASE_URL: "https://moody-candies-raise.loca.lt/auth"
+    API_BASE_URL: "https://tough-garlics-flash.loca.lt/api",
+    AUTH_BASE_URL: "https://tough-garlics-flash.loca.lt/auth"
 }
