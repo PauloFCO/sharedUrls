2659475086654a8e72cc1b4843367d2cd02d6520
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 08:33:11 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://red-crews-fly.loca.lt/api",
-    AUTH_BASE_URL: "https://red-crews-fly.loca.lt/auth"
+    API_BASE_URL: "https://salty-hornets-flash.loca.lt/api",
+    AUTH_BASE_URL: "https://salty-hornets-flash.loca.lt/auth"
 }
