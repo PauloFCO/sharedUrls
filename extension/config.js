7cd7d42ef9646af0f2857033fc71aaf5896899e4
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 14:35:25 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://warm-carrots-send.loca.lt/api",
-    AUTH_BASE_URL: "https://warm-carrots-send.loca.lt/auth"
+    API_BASE_URL: "https://eleven-jeans-accept.loca.lt/api",
+    AUTH_BASE_URL: "https://eleven-jeans-accept.loca.lt/auth"
 }
