d52910ded52593d6d56b3d4e3ea998efbb979445
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 12:34:37 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://upset-sites-show.loca.lt/api",
-    AUTH_BASE_URL: "https://upset-sites-show.loca.lt/auth"
+    API_BASE_URL: "https://warm-carrots-send.loca.lt/api",
+    AUTH_BASE_URL: "https://warm-carrots-send.loca.lt/auth"
 }
