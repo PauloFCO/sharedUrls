af9f84dfd20d97373ab19e30a7ff998aeefd00b6
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 02:31:03 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://sharp-horses-brake.loca.lt/api",
-    AUTH_BASE_URL: "https://sharp-horses-brake.loca.lt/auth"
+    API_BASE_URL: "https://polite-mice-sleep.loca.lt/api",
+    AUTH_BASE_URL: "https://polite-mice-sleep.loca.lt/auth"
 }
