930e2597037eee16a422a50019c67c78a45da09e
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 20:37:33 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://tricky-waves-feel.loca.lt/api",
-    AUTH_BASE_URL: "https://tricky-waves-feel.loca.lt/auth"
+    API_BASE_URL: "https://plenty-sloths-hide.loca.lt/api",
+    AUTH_BASE_URL: "https://plenty-sloths-hide.loca.lt/auth"
 }
