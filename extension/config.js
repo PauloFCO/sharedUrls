66e1492699ce9dd2a27c65c55a0c648af2d8a30a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 22:38:16 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://plenty-sloths-hide.loca.lt/api",
-    AUTH_BASE_URL: "https://plenty-sloths-hide.loca.lt/auth"
+    API_BASE_URL: "https://strong-insects-care.loca.lt/api",
+    AUTH_BASE_URL: "https://strong-insects-care.loca.lt/auth"
 }
