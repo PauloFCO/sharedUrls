fd69bc6d2794227838e55dec7f4945cb79af6c3a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 06:32:28 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clever-otters-stick.loca.lt/api",
-    AUTH_BASE_URL: "https://clever-otters-stick.loca.lt/auth"
+    API_BASE_URL: "https://red-crews-fly.loca.lt/api",
+    AUTH_BASE_URL: "https://red-crews-fly.loca.lt/auth"
 }
