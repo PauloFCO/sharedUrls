f227963367489636d288ff03452caff9d4ff6a92
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 10:33:54 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://salty-hornets-flash.loca.lt/api",
-    AUTH_BASE_URL: "https://salty-hornets-flash.loca.lt/auth"
+    API_BASE_URL: "https://upset-sites-show.loca.lt/api",
+    AUTH_BASE_URL: "https://upset-sites-show.loca.lt/auth"
 }
