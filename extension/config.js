c17c57e5bba955aaab77d93737bd024b035f6198
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 18:36:51 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://olive-hornets-sink.loca.lt/api",
-    AUTH_BASE_URL: "https://olive-hornets-sink.loca.lt/auth"
+    API_BASE_URL: "https://tricky-waves-feel.loca.lt/api",
+    AUTH_BASE_URL: "https://tricky-waves-feel.loca.lt/auth"
 }
