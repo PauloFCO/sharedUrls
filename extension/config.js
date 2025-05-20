55f32d0db23e6d7bab8a7f0e934828d8b8bcb256
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-20 00:30:19 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://four-planes-kick.loca.lt/api",
-    AUTH_BASE_URL: "https://four-planes-kick.loca.lt/auth"
+    API_BASE_URL: "https://sharp-horses-brake.loca.lt/api",
+    AUTH_BASE_URL: "https://sharp-horses-brake.loca.lt/auth"
 }
