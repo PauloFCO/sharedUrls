611eb9f14a0af5f8a2eb5049be27bbc294a3fe56
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-21 12:43:16 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://nasty-berries-chew.loca.lt/api",
-    AUTH_BASE_URL: "https://nasty-berries-chew.loca.lt/auth"
+    API_BASE_URL: "https://empty-fans-act.loca.lt/api",
+    AUTH_BASE_URL: "https://empty-fans-act.loca.lt/auth"
 }
