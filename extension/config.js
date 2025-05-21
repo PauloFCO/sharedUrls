6325e859e50dc9efe7546f3f934535f6267d61b3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-21 00:38:59 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://strong-insects-care.loca.lt/api",
-    AUTH_BASE_URL: "https://strong-insects-care.loca.lt/auth"
+    API_BASE_URL: "https://nine-spiders-own.loca.lt/api",
+    AUTH_BASE_URL: "https://nine-spiders-own.loca.lt/auth"
 }
