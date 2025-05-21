c06ad301beba02b91e6bebf2abe9b259785c9b5a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-21 16:44:42 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://dry-cases-stare.loca.lt/api",
-    AUTH_BASE_URL: "https://dry-cases-stare.loca.lt/auth"
+    API_BASE_URL: "https://fine-bats-attack.loca.lt/api",
+    AUTH_BASE_URL: "https://fine-bats-attack.loca.lt/auth"
 }
