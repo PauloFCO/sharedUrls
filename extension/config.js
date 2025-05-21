97212d2127e96f19ffb1dddfca4a5ae5db347845
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-21 06:41:08 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://khaki-schools-speak.loca.lt/api",
-    AUTH_BASE_URL: "https://khaki-schools-speak.loca.lt/auth"
+    API_BASE_URL: "https://strong-lights-report.loca.lt/api",
+    AUTH_BASE_URL: "https://strong-lights-report.loca.lt/auth"
 }
