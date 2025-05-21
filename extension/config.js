f747e9a981f8c192ad685b904663e9856fafd351
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-21 04:40:25 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://pink-flies-smile.loca.lt/api",
-    AUTH_BASE_URL: "https://pink-flies-smile.loca.lt/auth"
+    API_BASE_URL: "https://khaki-schools-speak.loca.lt/api",
+    AUTH_BASE_URL: "https://khaki-schools-speak.loca.lt/auth"
 }
