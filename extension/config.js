c2dc3e5d80a4256cd690207b0e5caf8dd73f3aaf
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-21 22:46:51 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://deep-falcons-act.loca.lt/api",
-    AUTH_BASE_URL: "https://deep-falcons-act.loca.lt/auth"
+    API_BASE_URL: "https://itchy-crabs-hope.loca.lt/api",
+    AUTH_BASE_URL: "https://itchy-crabs-hope.loca.lt/auth"
 }
