5288a73531dc9e7779fe27e15e6b5b659561cec1
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-22 20:54:44 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://busy-turkeys-fix.loca.lt/api",
-    AUTH_BASE_URL: "https://busy-turkeys-fix.loca.lt/auth"
+    API_BASE_URL: "https://tall-snakes-travel.loca.lt/api",
+    AUTH_BASE_URL: "https://tall-snakes-travel.loca.lt/auth"
 }
