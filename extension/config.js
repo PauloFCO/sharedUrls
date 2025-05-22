0d776cab18b70c72f0c5d7a4777bc432e59f1afc
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-22 08:50:26 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://floppy-emus-thank.loca.lt/api",
-    AUTH_BASE_URL: "https://floppy-emus-thank.loca.lt/auth"
+    API_BASE_URL: "https://quiet-pianos-go.loca.lt/api",
+    AUTH_BASE_URL: "https://quiet-pianos-go.loca.lt/auth"
 }
