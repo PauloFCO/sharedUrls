5250db36a4cfecd39c3554a16c1b2c2c005e711c
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-22 12:51:52 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://silver-clouds-slide.loca.lt/api",
-    AUTH_BASE_URL: "https://silver-clouds-slide.loca.lt/auth"
+    API_BASE_URL: "https://lazy-birds-end.loca.lt/api",
+    AUTH_BASE_URL: "https://lazy-birds-end.loca.lt/auth"
 }
