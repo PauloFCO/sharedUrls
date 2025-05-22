75e0cc136770eb3dec70264f3d2550f42094f089
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-22 14:52:35 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://lazy-birds-end.loca.lt/api",
-    AUTH_BASE_URL: "https://lazy-birds-end.loca.lt/auth"
+    API_BASE_URL: "https://giant-beans-type.loca.lt/api",
+    AUTH_BASE_URL: "https://giant-beans-type.loca.lt/auth"
 }
