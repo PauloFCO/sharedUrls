bd5eb5318307977b577885408ce7d12a2840c2df
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-23 19:02:36 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://nice-masks-wait.loca.lt/api",
-    AUTH_BASE_URL: "https://nice-masks-wait.loca.lt/auth"
+    API_BASE_URL: "https://free-games-sing.loca.lt/api",
+    AUTH_BASE_URL: "https://free-games-sing.loca.lt/auth"
 }
