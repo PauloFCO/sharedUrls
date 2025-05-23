06b7383b31eb5a7a3540465d1137054d1a17ad9f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-23 10:59:44 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://odd-dryers-stop.loca.lt/api",
-    AUTH_BASE_URL: "https://odd-dryers-stop.loca.lt/auth"
+    API_BASE_URL: "https://fast-banks-switch.loca.lt/api",
+    AUTH_BASE_URL: "https://fast-banks-switch.loca.lt/auth"
 }
