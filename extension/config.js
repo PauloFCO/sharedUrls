5c6685e1c1fb5f83c1e9ac86a0b6932c1a675cee
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-23 21:03:19 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://free-games-sing.loca.lt/api",
-    AUTH_BASE_URL: "https://free-games-sing.loca.lt/auth"
+    API_BASE_URL: "https://big-ghosts-open.loca.lt/api",
+    AUTH_BASE_URL: "https://big-ghosts-open.loca.lt/auth"
 }
