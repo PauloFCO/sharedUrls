961adcf2b8626aabb8b47e14b517bb03700a8f4a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-24 21:11:55 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clever-loops-wink.loca.lt/api",
-    AUTH_BASE_URL: "https://clever-loops-wink.loca.lt/auth"
+    API_BASE_URL: "https://nasty-zebras-enjoy.loca.lt/api",
+    AUTH_BASE_URL: "https://nasty-zebras-enjoy.loca.lt/auth"
 }
