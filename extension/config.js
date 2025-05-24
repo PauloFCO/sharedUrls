9728e75bee08d9e6741e671567594869df2085aa
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-24 23:12:38 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://nasty-zebras-enjoy.loca.lt/api",
-    AUTH_BASE_URL: "https://nasty-zebras-enjoy.loca.lt/auth"
+    API_BASE_URL: "https://eighty-crews-drum.loca.lt/api",
+    AUTH_BASE_URL: "https://eighty-crews-drum.loca.lt/auth"
 }
