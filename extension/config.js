7522e0776a4045afe09a7410a660d3dcd7f01551
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-24 09:07:37 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://chubby-cobras-shine.loca.lt/api",
-    AUTH_BASE_URL: "https://chubby-cobras-shine.loca.lt/auth"
+    API_BASE_URL: "/api",
+    AUTH_BASE_URL: "/auth"
 }
