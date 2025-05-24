b07bdc6f52292da893c11d6a175a453f4f15e6c3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-24 15:09:46 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://heavy-pigs-cheat.loca.lt/api",
-    AUTH_BASE_URL: "https://heavy-pigs-cheat.loca.lt/auth"
+    API_BASE_URL: "https://fast-grapes-talk.loca.lt/api",
+    AUTH_BASE_URL: "https://fast-grapes-talk.loca.lt/auth"
 }
