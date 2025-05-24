f0bf3371021c5adc513a4116463ee40b42b474dd
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-24 19:11:12 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://giant-crews-think.loca.lt/api",
-    AUTH_BASE_URL: "https://giant-crews-think.loca.lt/auth"
+    API_BASE_URL: "https://clever-loops-wink.loca.lt/api",
+    AUTH_BASE_URL: "https://clever-loops-wink.loca.lt/auth"
 }
