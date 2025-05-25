5f73e46f8198d2aec0828d076293fc1eae79917c
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-25 07:15:29 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://gold-comics-drop.loca.lt/api",
-    AUTH_BASE_URL: "https://gold-comics-drop.loca.lt/auth"
+    API_BASE_URL: "https://tasty-parrots-check.loca.lt/api",
+    AUTH_BASE_URL: "https://tasty-parrots-check.loca.lt/auth"
 }
