9aae247a224c5c7c450587874cd79cf9513f0fbf
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-25 21:20:32 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://honest-mammals-poke.loca.lt/api",
-    AUTH_BASE_URL: "https://honest-mammals-poke.loca.lt/auth"
+    API_BASE_URL: "https://swift-colts-shave.loca.lt/api",
+    AUTH_BASE_URL: "https://swift-colts-shave.loca.lt/auth"
 }
