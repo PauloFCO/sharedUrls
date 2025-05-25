07546bb4e4958ece7453c9a1b86ab119c028540f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-25 09:16:12 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://tasty-parrots-check.loca.lt/api",
-    AUTH_BASE_URL: "https://tasty-parrots-check.loca.lt/auth"
+    API_BASE_URL: "https://floppy-coins-check.loca.lt/api",
+    AUTH_BASE_URL: "https://floppy-coins-check.loca.lt/auth"
 }
