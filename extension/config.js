e2c9aa6c25973f5f749943e107bc65157b083a43
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-25 05:14:47 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://eight-symbols-like.loca.lt/api",
-    AUTH_BASE_URL: "https://eight-symbols-like.loca.lt/auth"
+    API_BASE_URL: "https://gold-comics-drop.loca.lt/api",
+    AUTH_BASE_URL: "https://gold-comics-drop.loca.lt/auth"
 }
