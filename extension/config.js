bb4e873d54b9f5b7e7ca20097e8afb0a6dfc2c1c
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-25 19:19:47 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://chatty-regions-buy.loca.lt/api",
-    AUTH_BASE_URL: "https://chatty-regions-buy.loca.lt/auth"
+    API_BASE_URL: "https://honest-mammals-poke.loca.lt/api",
+    AUTH_BASE_URL: "https://honest-mammals-poke.loca.lt/auth"
 }
