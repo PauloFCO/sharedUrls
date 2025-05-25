eb1c6ca717b4ad35be13f1a5283cec38fce9ce7f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-25 03:14:04 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://young-cases-ring.loca.lt/api",
-    AUTH_BASE_URL: "https://young-cases-ring.loca.lt/auth"
+    API_BASE_URL: "https://eight-symbols-like.loca.lt/api",
+    AUTH_BASE_URL: "https://eight-symbols-like.loca.lt/auth"
 }
