a66f663d857c64ae2165569bca26d6dab4d99a9a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-26 13:26:15 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://whole-dodos-stay.loca.lt/api",
-    AUTH_BASE_URL: "https://whole-dodos-stay.loca.lt/auth"
+    API_BASE_URL: "https://strong-gifts-act.loca.lt/api",
+    AUTH_BASE_URL: "https://strong-gifts-act.loca.lt/auth"
 }
