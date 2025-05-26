bbbbcc5c21a45ed2af78f5c8ec490c5d288d21c3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-26 07:24:06 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://three-hornets-sip.loca.lt/api",
-    AUTH_BASE_URL: "https://three-hornets-sip.loca.lt/auth"
+    API_BASE_URL: "https://slimy-ants-begin.loca.lt/api",
+    AUTH_BASE_URL: "https://slimy-ants-begin.loca.lt/auth"
 }
