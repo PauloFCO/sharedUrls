c831e9e257dc94399a02d1fa220cc2b854e88825
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-26 01:21:58 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://free-cloths-laugh.loca.lt/api",
-    AUTH_BASE_URL: "https://free-cloths-laugh.loca.lt/auth"
+    API_BASE_URL: "https://eight-queens-relate.loca.lt/api",
+    AUTH_BASE_URL: "https://eight-queens-relate.loca.lt/auth"
 }
