9dad447a5b04ee5768de858d0759b584962d4599
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-26 21:29:07 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://odd-jobs-crash.loca.lt/api",
-    AUTH_BASE_URL: "https://odd-jobs-crash.loca.lt/auth"
+    API_BASE_URL: "https://short-candies-divide.loca.lt/api",
+    AUTH_BASE_URL: "https://short-candies-divide.loca.lt/auth"
 }
