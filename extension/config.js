2786ddc8a01cdd70f0e6a26f5c55abee3d8b94a7
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-26 09:24:49 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://slimy-ants-begin.loca.lt/api",
-    AUTH_BASE_URL: "https://slimy-ants-begin.loca.lt/auth"
+    API_BASE_URL: "https://metal-squids-pay.loca.lt/api",
+    AUTH_BASE_URL: "https://metal-squids-pay.loca.lt/auth"
 }
