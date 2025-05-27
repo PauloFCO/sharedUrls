8aec1b4e7bdb431e97153f18aed2a02ca372843b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-27 01:30:32 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clean-pens-thank.loca.lt/api",
-    AUTH_BASE_URL: "https://clean-pens-thank.loca.lt/auth"
+    API_BASE_URL: "https://fancy-shoes-itch.loca.lt/api",
+    AUTH_BASE_URL: "https://fancy-shoes-itch.loca.lt/auth"
 }
