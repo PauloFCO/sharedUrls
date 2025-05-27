082a05b88b9da37c86bceed4d291d6225e2e0d8a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-27 23:38:25 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://five-gifts-attack.loca.lt/api",
-    AUTH_BASE_URL: "https://five-gifts-attack.loca.lt/auth"
+    API_BASE_URL: "https://poor-spoons-tell.loca.lt/api",
+    AUTH_BASE_URL: "https://poor-spoons-tell.loca.lt/auth"
 }
