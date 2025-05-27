32112ef203b6591ddfa96198f03ee07ef1e88327
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-27 11:34:07 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://upset-llamas-mix.loca.lt/api",
-    AUTH_BASE_URL: "https://upset-llamas-mix.loca.lt/auth"
+    API_BASE_URL: "https://funny-clouds-grab.loca.lt/api",
+    AUTH_BASE_URL: "https://funny-clouds-grab.loca.lt/auth"
 }
