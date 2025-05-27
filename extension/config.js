95e6452d56e18cef466a01048e3a7287db6fcccf
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-27 15:35:33 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://young-jars-film.loca.lt/api",
-    AUTH_BASE_URL: "https://young-jars-film.loca.lt/auth"
+    API_BASE_URL: "https://khaki-pots-doubt.loca.lt/api",
+    AUTH_BASE_URL: "https://khaki-pots-doubt.loca.lt/auth"
 }
