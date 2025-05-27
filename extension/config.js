d8801d81f731d2055979f7e35fd03c48620643eb
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-27 21:37:42 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://hot-lamps-bake.loca.lt/api",
-    AUTH_BASE_URL: "https://hot-lamps-bake.loca.lt/auth"
+    API_BASE_URL: "https://five-gifts-attack.loca.lt/api",
+    AUTH_BASE_URL: "https://five-gifts-attack.loca.lt/auth"
 }
