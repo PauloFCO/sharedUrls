7510505e2bfc76870ba988a070f42e73193a8e19
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-27 17:36:16 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://khaki-pots-doubt.loca.lt/api",
-    AUTH_BASE_URL: "https://khaki-pots-doubt.loca.lt/auth"
+    API_BASE_URL: "https://clever-kids-pump.loca.lt/api",
+    AUTH_BASE_URL: "https://clever-kids-pump.loca.lt/auth"
 }
