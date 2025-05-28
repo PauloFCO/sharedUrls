fd75569fa8bd6bee7df7f07682d4257fbc9bb781
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-28 03:39:52 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://rude-comics-find.loca.lt/api",
-    AUTH_BASE_URL: "https://rude-comics-find.loca.lt/auth"
+    API_BASE_URL: "https://red-clouds-brake.loca.lt/api",
+    AUTH_BASE_URL: "https://red-clouds-brake.loca.lt/auth"
 }
