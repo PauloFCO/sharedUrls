4b5017c3da3e7072f85f5fab6e1aef9dc9c43750
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-28 05:40:35 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://red-clouds-brake.loca.lt/api",
-    AUTH_BASE_URL: "https://red-clouds-brake.loca.lt/auth"
+    API_BASE_URL: "https://honest-cats-exist.loca.lt/api",
+    AUTH_BASE_URL: "https://honest-cats-exist.loca.lt/auth"
 }
