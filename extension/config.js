f2221c3973957a734b936f529d618f96f8977692
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-28 21:46:19 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://four-cities-itch.loca.lt/api",
-    AUTH_BASE_URL: "https://four-cities-itch.loca.lt/auth"
+    API_BASE_URL: "https://fancy-melons-clap.loca.lt/api",
+    AUTH_BASE_URL: "https://fancy-melons-clap.loca.lt/auth"
 }
