b8e41ac63f688f86ebdddaea4d7b5b409ab6003d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-28 19:45:35 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fast-hotels-poke.loca.lt/api",
-    AUTH_BASE_URL: "https://fast-hotels-poke.loca.lt/auth"
+    API_BASE_URL: "https://four-cities-itch.loca.lt/api",
+    AUTH_BASE_URL: "https://four-cities-itch.loca.lt/auth"
 }
