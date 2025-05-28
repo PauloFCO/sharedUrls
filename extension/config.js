51ad32cfb3852303ae3045df76da95692bdbc609
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-28 07:41:18 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://honest-cats-exist.loca.lt/api",
-    AUTH_BASE_URL: "https://honest-cats-exist.loca.lt/auth"
+    API_BASE_URL: "https://lucky-paths-cheat.loca.lt/api",
+    AUTH_BASE_URL: "https://lucky-paths-cheat.loca.lt/auth"
 }
