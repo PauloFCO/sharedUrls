0e6a1db0132e7f29d74e94512afc06f20ece2985
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-28 01:39:09 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://poor-spoons-tell.loca.lt/api",
-    AUTH_BASE_URL: "https://poor-spoons-tell.loca.lt/auth"
+    API_BASE_URL: "https://rude-comics-find.loca.lt/api",
+    AUTH_BASE_URL: "https://rude-comics-find.loca.lt/auth"
 }
