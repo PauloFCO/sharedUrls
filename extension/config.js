0862bf2f3a744236502158a2220b438d3f1c61fb
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-28 17:44:52 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://rich-crabs-agree.loca.lt/api",
-    AUTH_BASE_URL: "https://rich-crabs-agree.loca.lt/auth"
+    API_BASE_URL: "https://fast-hotels-poke.loca.lt/api",
+    AUTH_BASE_URL: "https://fast-hotels-poke.loca.lt/auth"
 }
