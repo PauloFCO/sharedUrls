204d2f17d7e2dd6aa7f43b2a8f18974f8a91815a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-29 15:52:46 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://red-carpets-happen.loca.lt/api",
-    AUTH_BASE_URL: "https://red-carpets-happen.loca.lt/auth"
+    API_BASE_URL: "https://eleven-dragons-smash.loca.lt/api",
+    AUTH_BASE_URL: "https://eleven-dragons-smash.loca.lt/auth"
 }
