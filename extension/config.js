7d432fbd2db9ce54e8efb207fa1b82f879a25c3d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-29 09:50:37 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://gold-donuts-vanish.loca.lt/api",
-    AUTH_BASE_URL: "https://gold-donuts-vanish.loca.lt/auth"
+    API_BASE_URL: "https://cold-carrots-pay.loca.lt/api",
+    AUTH_BASE_URL: "https://cold-carrots-pay.loca.lt/auth"
 }
