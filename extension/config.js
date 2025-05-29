226d4cd2573bce5d6c279b23fd80e793300076b2
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-29 11:51:20 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://cold-carrots-pay.loca.lt/api",
-    AUTH_BASE_URL: "https://cold-carrots-pay.loca.lt/auth"
+    API_BASE_URL: "https://chilly-rings-fry.loca.lt/api",
+    AUTH_BASE_URL: "https://chilly-rings-fry.loca.lt/auth"
 }
