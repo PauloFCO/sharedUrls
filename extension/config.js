35b83286069ce5f2f5aa3b84dd65a111126d62da
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-29 03:48:29 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://thirty-years-sink.loca.lt/api",
-    AUTH_BASE_URL: "https://thirty-years-sink.loca.lt/auth"
+    API_BASE_URL: "https://ten-memes-greet.loca.lt/api",
+    AUTH_BASE_URL: "https://ten-memes-greet.loca.lt/auth"
 }
