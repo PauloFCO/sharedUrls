da81b7de6dbb90b665763aed2a37a85a35f1c479
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-30 22:03:29 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://short-ads-bow.loca.lt/api",
-    AUTH_BASE_URL: "https://short-ads-bow.loca.lt/auth"
+    API_BASE_URL: "https://curly-waves-judge.loca.lt/api",
+    AUTH_BASE_URL: "https://curly-waves-judge.loca.lt/auth"
 }
