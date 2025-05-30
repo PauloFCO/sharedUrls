23e9ff036f11f0039176bf4ddeaa64fa34152a35
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-30 09:59:11 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://puny-peas-poke.loca.lt/api",
-    AUTH_BASE_URL: "https://puny-peas-poke.loca.lt/auth"
+    API_BASE_URL: "https://dark-rooms-hide.loca.lt/api",
+    AUTH_BASE_URL: "https://dark-rooms-hide.loca.lt/auth"
 }
