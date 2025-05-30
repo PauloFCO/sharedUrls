d55351f20ca3ae85f43a5fb1e914ac0ff08332e0
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-30 18:02:03 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fresh-hounds-marry.loca.lt/api",
-    AUTH_BASE_URL: "https://fresh-hounds-marry.loca.lt/auth"
+    API_BASE_URL: "https://fruity-flies-vanish.loca.lt/api",
+    AUTH_BASE_URL: "https://fruity-flies-vanish.loca.lt/auth"
 }
