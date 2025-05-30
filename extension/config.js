15f8e9c25177a6816558b6b008594808978006cf
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-30 05:57:46 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://long-shirts-flow.loca.lt/api",
-    AUTH_BASE_URL: "https://long-shirts-flow.loca.lt/auth"
+    API_BASE_URL: "/api",
+    AUTH_BASE_URL: "/auth"
 }
