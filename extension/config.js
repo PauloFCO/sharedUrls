01c25de3ae21b13aa2ba8633bc3053d824f406e6
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-31 14:09:11 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clean-nails-listen.loca.lt/api",
-    AUTH_BASE_URL: "https://clean-nails-listen.loca.lt/auth"
+    API_BASE_URL: "https://dirty-nights-train.loca.lt/api",
+    AUTH_BASE_URL: "https://dirty-nights-train.loca.lt/auth"
 }
