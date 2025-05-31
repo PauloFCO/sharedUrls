46a8b1cab82d17fc768737e33bce03f0dc8c4df4
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-31 10:07:45 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://ninety-sheep-glow.loca.lt/api",
-    AUTH_BASE_URL: "https://ninety-sheep-glow.loca.lt/auth"
+    API_BASE_URL: "https://pretty-rockets-pay.loca.lt/api",
+    AUTH_BASE_URL: "https://pretty-rockets-pay.loca.lt/auth"
 }
