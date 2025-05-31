abea6120404ab3c9436cc3023a5cac3f30290880
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-31 06:06:20 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://funny-turtles-count.loca.lt/api",
-    AUTH_BASE_URL: "https://funny-turtles-count.loca.lt/auth"
+    API_BASE_URL: "https://tall-clocks-eat.loca.lt/api",
+    AUTH_BASE_URL: "https://tall-clocks-eat.loca.lt/auth"
 }
