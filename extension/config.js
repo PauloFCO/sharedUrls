26510a8a5f0f6d67ade6f8fa4997455b15a9a70d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-31 22:12:03 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://sweet-windows-peel.loca.lt/api",
-    AUTH_BASE_URL: "https://sweet-windows-peel.loca.lt/auth"
+    API_BASE_URL: "https://quick-moose-film.loca.lt/api",
+    AUTH_BASE_URL: "https://quick-moose-film.loca.lt/auth"
 }
