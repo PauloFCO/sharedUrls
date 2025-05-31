e9008483818e2f83ae69c06bf8ef9327c76cd068
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-31 18:10:37 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://curvy-turtles-worry.loca.lt/api",
-    AUTH_BASE_URL: "https://curvy-turtles-worry.loca.lt/auth"
+    API_BASE_URL: "https://hip-zoos-drive.loca.lt/api",
+    AUTH_BASE_URL: "https://hip-zoos-drive.loca.lt/auth"
 }
