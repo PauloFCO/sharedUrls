9bf56f08798ef6199dc13c5c7eeb4c85ff7d7623
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-31 12:08:28 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://pretty-rockets-pay.loca.lt/api",
-    AUTH_BASE_URL: "https://pretty-rockets-pay.loca.lt/auth"
+    API_BASE_URL: "https://clean-nails-listen.loca.lt/api",
+    AUTH_BASE_URL: "https://clean-nails-listen.loca.lt/auth"
 }
