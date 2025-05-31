3f2d75301a1bb7c0d7d84c575f35f30e2ee2034d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-31 08:07:03 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://tall-clocks-eat.loca.lt/api",
-    AUTH_BASE_URL: "https://tall-clocks-eat.loca.lt/auth"
+    API_BASE_URL: "https://ninety-sheep-glow.loca.lt/api",
+    AUTH_BASE_URL: "https://ninety-sheep-glow.loca.lt/auth"
 }
