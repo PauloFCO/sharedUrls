ba26b6253737fdd3d2baf5a612dee1778fa3a2de
PauloFCO <pauloqueteiglesias@gmail.com>
2025-05-31 16:09:54 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://dirty-nights-train.loca.lt/api",
-    AUTH_BASE_URL: "https://dirty-nights-train.loca.lt/auth"
+    API_BASE_URL: "https://curvy-turtles-worry.loca.lt/api",
+    AUTH_BASE_URL: "https://curvy-turtles-worry.loca.lt/auth"
 }
