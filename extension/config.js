ce787a30c1c4f245333b5e6c4226bf617b7a6ede
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-01 00:12:45 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://quick-moose-film.loca.lt/api",
-    AUTH_BASE_URL: "https://quick-moose-film.loca.lt/auth"
+    API_BASE_URL: "https://ninety-plums-behave.loca.lt/api",
+    AUTH_BASE_URL: "https://ninety-plums-behave.loca.lt/auth"
 }
