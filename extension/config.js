ad9955d989495ebb92154d3d41dde6edeb4bafde
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-01 16:18:28 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://ripe-rabbits-hunt.loca.lt/api",
-    AUTH_BASE_URL: "https://ripe-rabbits-hunt.loca.lt/auth"
+    API_BASE_URL: "https://bumpy-meals-repair.loca.lt/api",
+    AUTH_BASE_URL: "https://bumpy-meals-repair.loca.lt/auth"
 }
