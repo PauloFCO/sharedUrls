8ad5d82f3449f4e8cdf1ed351eced0bed255fd6d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-01 18:19:11 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://bumpy-meals-repair.loca.lt/api",
-    AUTH_BASE_URL: "https://bumpy-meals-repair.loca.lt/auth"
+    API_BASE_URL: "https://hot-cameras-dream.loca.lt/api",
+    AUTH_BASE_URL: "https://hot-cameras-dream.loca.lt/auth"
 }
