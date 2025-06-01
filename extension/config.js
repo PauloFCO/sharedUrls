ae96142ea102343988c8a0f4d2fb5cb6a786a3f8
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-01 10:16:20 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://brave-poems-carry.loca.lt/api",
-    AUTH_BASE_URL: "https://brave-poems-carry.loca.lt/auth"
+    API_BASE_URL: "https://ten-nails-end.loca.lt/api",
+    AUTH_BASE_URL: "https://ten-nails-end.loca.lt/auth"
 }
