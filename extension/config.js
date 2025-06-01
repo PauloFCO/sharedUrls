133eb4366a750f9bf8531d9b34c55ab33d588df5
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-01 08:15:37 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://bumpy-brooms-post.loca.lt/api",
-    AUTH_BASE_URL: "https://bumpy-brooms-post.loca.lt/auth"
+    API_BASE_URL: "https://brave-poems-carry.loca.lt/api",
+    AUTH_BASE_URL: "https://brave-poems-carry.loca.lt/auth"
 }
