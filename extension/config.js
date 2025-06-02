9c3b9bfc2e8d9e2d5eabc462c7eca0ec412c1c27
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-02 22:29:13 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://smart-readers-bake.loca.lt/api",
-    AUTH_BASE_URL: "https://smart-readers-bake.loca.lt/auth"
+    API_BASE_URL: "https://social-ghosts-smoke.loca.lt/api",
+    AUTH_BASE_URL: "https://social-ghosts-smoke.loca.lt/auth"
 }
