0722a851f4c23d550446d8394d5488d0c310718b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-02 04:22:46 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://tidy-onions-sin.loca.lt/api",
-    AUTH_BASE_URL: "https://tidy-onions-sin.loca.lt/auth"
+    API_BASE_URL: "https://silver-papayas-exist.loca.lt/api",
+    AUTH_BASE_URL: "https://silver-papayas-exist.loca.lt/auth"
 }
