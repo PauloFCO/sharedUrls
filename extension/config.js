3a2d81ba24f0cce55d397a917b7d1bbaabee3a04
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-02 18:27:47 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://honest-fans-grow.loca.lt/api",
-    AUTH_BASE_URL: "https://honest-fans-grow.loca.lt/auth"
+    API_BASE_URL: "https://grumpy-pigs-kneel.loca.lt/api",
+    AUTH_BASE_URL: "https://grumpy-pigs-kneel.loca.lt/auth"
 }
