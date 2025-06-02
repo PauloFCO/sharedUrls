ced7c16d331223d137b8786b03b45a91b973f976
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-02 08:24:12 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://violet-readers-shave.loca.lt/api",
-    AUTH_BASE_URL: "https://violet-readers-shave.loca.lt/auth"
+    API_BASE_URL: "https://plain-insects-search.loca.lt/api",
+    AUTH_BASE_URL: "https://plain-insects-search.loca.lt/auth"
 }
