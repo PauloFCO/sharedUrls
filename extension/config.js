792d04338ee16f8943a5118745cac821be727e0d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-02 16:27:04 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://clever-insects-lay.loca.lt/api",
-    AUTH_BASE_URL: "https://clever-insects-lay.loca.lt/auth"
+    API_BASE_URL: "https://honest-fans-grow.loca.lt/api",
+    AUTH_BASE_URL: "https://honest-fans-grow.loca.lt/auth"
 }
