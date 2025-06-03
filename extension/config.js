4094ee678d686cfb6c6ebb4e0252828b2750926b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-03 20:37:04 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fluffy-horses-eat.loca.lt/api",
-    AUTH_BASE_URL: "https://fluffy-horses-eat.loca.lt/auth"
+    API_BASE_URL: "https://legal-poets-join.loca.lt/api",
+    AUTH_BASE_URL: "https://legal-poets-join.loca.lt/auth"
 }
