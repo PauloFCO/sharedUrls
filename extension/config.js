22ede0be4d4564bc64d28693333b2685009ddb17
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-03 06:32:04 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://shy-beds-slide.loca.lt/api",
-    AUTH_BASE_URL: "https://shy-beds-slide.loca.lt/auth"
+    API_BASE_URL: "https://itchy-tires-search.loca.lt/api",
+    AUTH_BASE_URL: "https://itchy-tires-search.loca.lt/auth"
 }
