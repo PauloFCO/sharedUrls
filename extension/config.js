8adcb533bd7bfa5ed9f49dc8f4fc84b03889cb4c
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-03 04:31:21 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://thin-friends-punch.loca.lt/api",
-    AUTH_BASE_URL: "https://thin-friends-punch.loca.lt/auth"
+    API_BASE_URL: "https://shy-beds-slide.loca.lt/api",
+    AUTH_BASE_URL: "https://shy-beds-slide.loca.lt/auth"
 }
