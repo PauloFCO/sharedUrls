22aa7f1de781129a4c7b8bf90dd619f7310d466b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-03 16:35:39 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "/api",
-    AUTH_BASE_URL: "/auth"
+    API_BASE_URL: "https://yellow-times-go.loca.lt/api",
+    AUTH_BASE_URL: "https://yellow-times-go.loca.lt/auth"
 }
