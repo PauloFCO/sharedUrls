5cd38d41980c375c5059eb299ac169d3967846c4
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-03 02:30:38 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://short-toys-peel.loca.lt/api",
-    AUTH_BASE_URL: "https://short-toys-peel.loca.lt/auth"
+    API_BASE_URL: "https://thin-friends-punch.loca.lt/api",
+    AUTH_BASE_URL: "https://thin-friends-punch.loca.lt/auth"
 }
