4cb167c08add3520500679cdb6b05da998a9ae10
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-04 18:44:57 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://brave-weeks-create.loca.lt/api",
-    AUTH_BASE_URL: "https://brave-weeks-create.loca.lt/auth"
+    API_BASE_URL: "https://hip-chefs-camp.loca.lt/api",
+    AUTH_BASE_URL: "https://hip-chefs-camp.loca.lt/auth"
 }
