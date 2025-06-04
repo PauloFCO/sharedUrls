61110d17737dd7f0caf554df3da3594564025890
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-04 00:38:30 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://better-days-start.loca.lt/api",
-    AUTH_BASE_URL: "https://better-days-start.loca.lt/auth"
+    API_BASE_URL: "https://fancy-loops-arrive.loca.lt/api",
+    AUTH_BASE_URL: "https://fancy-loops-arrive.loca.lt/auth"
 }
