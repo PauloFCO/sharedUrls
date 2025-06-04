ac3e736efb2b47d28dbef1478441a6bcd8cf0be3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-04 16:44:14 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://free-spoons-spend.loca.lt/api",
-    AUTH_BASE_URL: "https://free-spoons-spend.loca.lt/auth"
+    API_BASE_URL: "https://brave-weeks-create.loca.lt/api",
+    AUTH_BASE_URL: "https://brave-weeks-create.loca.lt/auth"
 }
