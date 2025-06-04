4f2b47bed88f22ce8cbc4b1d7af2c72b3fa2afa8
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-04 04:39:56 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://dark-spoons-sip.loca.lt/api",
-    AUTH_BASE_URL: "https://dark-spoons-sip.loca.lt/auth"
+    API_BASE_URL: "https://small-nails-dream.loca.lt/api",
+    AUTH_BASE_URL: "https://small-nails-dream.loca.lt/auth"
 }
