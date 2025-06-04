72c9678404baef2487c201752af9788505a05b13
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-04 10:42:05 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://twelve-squids-doubt.loca.lt/api",
-    AUTH_BASE_URL: "https://twelve-squids-doubt.loca.lt/auth"
+    API_BASE_URL: "https://short-sheep-add.loca.lt/api",
+    AUTH_BASE_URL: "https://short-sheep-add.loca.lt/auth"
 }
