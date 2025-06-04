7395808da67e15c35a3c7bf57955371a96843340
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-04 12:42:48 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://short-sheep-add.loca.lt/api",
-    AUTH_BASE_URL: "https://short-sheep-add.loca.lt/auth"
+    API_BASE_URL: "https://tasty-times-happen.loca.lt/api",
+    AUTH_BASE_URL: "https://tasty-times-happen.loca.lt/auth"
 }
