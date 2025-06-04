f5ffff003ab2098d69a02af914482c39ea0fe56d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-04 20:45:39 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://hip-chefs-camp.loca.lt/api",
-    AUTH_BASE_URL: "https://hip-chefs-camp.loca.lt/auth"
+    API_BASE_URL: "https://few-bottles-sip.loca.lt/api",
+    AUTH_BASE_URL: "https://few-bottles-sip.loca.lt/auth"
 }
