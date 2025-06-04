216a63b16537cbf7bda317d790047bed72769fc7
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-04 22:46:22 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://few-bottles-sip.loca.lt/api",
-    AUTH_BASE_URL: "https://few-bottles-sip.loca.lt/auth"
+    API_BASE_URL: "https://four-ears-unite.loca.lt/api",
+    AUTH_BASE_URL: "https://four-ears-unite.loca.lt/auth"
 }
