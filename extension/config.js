792b2e373ac1a5e707a4433560c28ca0c5885e8b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-05 10:50:40 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://open-numbers-learn.loca.lt/api",
-    AUTH_BASE_URL: "https://open-numbers-learn.loca.lt/auth"
+    API_BASE_URL: "https://metal-bats-shout.loca.lt/api",
+    AUTH_BASE_URL: "https://metal-bats-shout.loca.lt/auth"
 }
