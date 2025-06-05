d170af4abc10f2c9efcc9bf5177e7e35a3ce350c
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-05 12:51:23 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://metal-bats-shout.loca.lt/api",
-    AUTH_BASE_URL: "https://metal-bats-shout.loca.lt/auth"
+    API_BASE_URL: "https://old-melons-remain.loca.lt/api",
+    AUTH_BASE_URL: "https://old-melons-remain.loca.lt/auth"
 }
