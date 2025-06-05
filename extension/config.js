24111b361cdccb2866d4faa537de0eeacc564117
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-05 22:54:58 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://twenty-clowns-type.loca.lt/api",
-    AUTH_BASE_URL: "https://twenty-clowns-type.loca.lt/auth"
+    API_BASE_URL: "https://odd-rooms-occur.loca.lt/api",
+    AUTH_BASE_URL: "https://odd-rooms-occur.loca.lt/auth"
 }
