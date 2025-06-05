51efd57689647b4ea2fd8e0ada0c7813531ab88f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-05 00:47:05 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://four-ears-unite.loca.lt/api",
-    AUTH_BASE_URL: "https://four-ears-unite.loca.lt/auth"
+    API_BASE_URL: "https://thin-toes-teach.loca.lt/api",
+    AUTH_BASE_URL: "https://thin-toes-teach.loca.lt/auth"
 }
