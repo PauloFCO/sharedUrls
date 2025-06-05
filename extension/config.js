b0fd287aa56fe07558b7b77fd678f3d315e61a3a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-05 02:47:48 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://thin-toes-teach.loca.lt/api",
-    AUTH_BASE_URL: "https://thin-toes-teach.loca.lt/auth"
+    API_BASE_URL: "https://green-results-teach.loca.lt/api",
+    AUTH_BASE_URL: "https://green-results-teach.loca.lt/auth"
 }
