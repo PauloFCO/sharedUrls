6b3aff5c75aa3f3a7f584538fbfe23cd6c256c1e
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-05 08:49:57 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://salty-lies-cheer.loca.lt/api",
-    AUTH_BASE_URL: "https://salty-lies-cheer.loca.lt/auth"
+    API_BASE_URL: "https://open-numbers-learn.loca.lt/api",
+    AUTH_BASE_URL: "https://open-numbers-learn.loca.lt/auth"
 }
