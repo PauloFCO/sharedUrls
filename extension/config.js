dbe1274f757e070431336a1a6cdc8048721d4ad9
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-05 06:49:14 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://itchy-bars-drive.loca.lt/api",
-    AUTH_BASE_URL: "https://itchy-bars-drive.loca.lt/auth"
+    API_BASE_URL: "https://salty-lies-cheer.loca.lt/api",
+    AUTH_BASE_URL: "https://salty-lies-cheer.loca.lt/auth"
 }
