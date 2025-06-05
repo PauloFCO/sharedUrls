f3cc79466f1c1fbcc45ad855c30da5bc6aea10e6
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-05 04:48:31 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://green-results-teach.loca.lt/api",
-    AUTH_BASE_URL: "https://green-results-teach.loca.lt/auth"
+    API_BASE_URL: "https://itchy-bars-drive.loca.lt/api",
+    AUTH_BASE_URL: "https://itchy-bars-drive.loca.lt/auth"
 }
