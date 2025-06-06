0e4c29375d86dd79c14e29af813e5eef98a8244d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-06 08:58:33 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://tidy-suits-shave.loca.lt/api",
-    AUTH_BASE_URL: "https://tidy-suits-shave.loca.lt/auth"
+    API_BASE_URL: "https://blue-books-roll.loca.lt/api",
+    AUTH_BASE_URL: "https://blue-books-roll.loca.lt/auth"
 }
