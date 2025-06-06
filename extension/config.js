921973ce8c41476a4f5c0d1d8ccd5e345cdfda5b
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-06 02:56:24 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://silent-colts-joke.loca.lt/api",
-    AUTH_BASE_URL: "https://silent-colts-joke.loca.lt/auth"
+    API_BASE_URL: "https://dull-planes-punch.loca.lt/api",
+    AUTH_BASE_URL: "https://dull-planes-punch.loca.lt/auth"
 }
