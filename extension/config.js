dddd53edea5e631cb9e655a7e235daefc55dea7d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-06 23:03:34 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://spicy-spiders-remain.loca.lt/api",
-    AUTH_BASE_URL: "https://spicy-spiders-remain.loca.lt/auth"
+    API_BASE_URL: "https://hungry-kings-laugh.loca.lt/api",
+    AUTH_BASE_URL: "https://hungry-kings-laugh.loca.lt/auth"
 }
