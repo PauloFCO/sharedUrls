f124e2ba69e94a7a1c0d3efde3d17a4d5511319a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-06 12:59:59 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://violet-streets-peel.loca.lt/api",
-    AUTH_BASE_URL: "https://violet-streets-peel.loca.lt/auth"
+    API_BASE_URL: "https://clever-lines-juggle.loca.lt/api",
+    AUTH_BASE_URL: "https://clever-lines-juggle.loca.lt/auth"
 }
