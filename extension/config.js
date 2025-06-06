6ea12169843d7b3192d6a1fbdca88bdd951e492d
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-06 10:59:16 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://blue-books-roll.loca.lt/api",
-    AUTH_BASE_URL: "https://blue-books-roll.loca.lt/auth"
+    API_BASE_URL: "https://violet-streets-peel.loca.lt/api",
+    AUTH_BASE_URL: "https://violet-streets-peel.loca.lt/auth"
 }
