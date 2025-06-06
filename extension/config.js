b607f813041aa5490b4f8947230885fabb66de02
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-06 19:02:07 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://strong-taxis-shout.loca.lt/api",
-    AUTH_BASE_URL: "https://strong-taxis-shout.loca.lt/auth"
+    API_BASE_URL: "/api",
+    AUTH_BASE_URL: "/auth"
 }
