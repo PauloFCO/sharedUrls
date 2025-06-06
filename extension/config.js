ca9958c94d78ab03bf18a2a2d6a004ad326f1576
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-06 04:57:07 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://dull-planes-punch.loca.lt/api",
-    AUTH_BASE_URL: "https://dull-planes-punch.loca.lt/auth"
+    API_BASE_URL: "https://bright-badgers-wave.loca.lt/api",
+    AUTH_BASE_URL: "https://bright-badgers-wave.loca.lt/auth"
 }
