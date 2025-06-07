88385b5be8ed6d8fe362ec68234245aebf4941d9
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-07 13:08:34 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://lucky-streets-make.loca.lt/api",
-    AUTH_BASE_URL: "https://lucky-streets-make.loca.lt/auth"
+    API_BASE_URL: "https://chilly-jobs-attend.loca.lt/api",
+    AUTH_BASE_URL: "https://chilly-jobs-attend.loca.lt/auth"
 }
