6e0e3fe85c27fb0418ee3f88b58326d79f02b067
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-07 11:07:51 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://bumpy-parents-beg.loca.lt/api",
-    AUTH_BASE_URL: "https://bumpy-parents-beg.loca.lt/auth"
+    API_BASE_URL: "https://lucky-streets-make.loca.lt/api",
+    AUTH_BASE_URL: "https://lucky-streets-make.loca.lt/auth"
 }
