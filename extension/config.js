785f5e51d97ae3e087738d7bf89007591c2993b8
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-07 21:11:25 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://wide-ants-take.loca.lt/api",
-    AUTH_BASE_URL: "https://wide-ants-take.loca.lt/auth"
+    API_BASE_URL: "https://large-buttons-matter.loca.lt/api",
+    AUTH_BASE_URL: "https://large-buttons-matter.loca.lt/auth"
 }
