e493e0fe950b6e8f797d48d42c94763416e0a36f
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-07 15:09:17 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://chilly-jobs-attend.loca.lt/api",
-    AUTH_BASE_URL: "https://chilly-jobs-attend.loca.lt/auth"
+    API_BASE_URL: "https://new-schools-accept.loca.lt/api",
+    AUTH_BASE_URL: "https://new-schools-accept.loca.lt/auth"
 }
