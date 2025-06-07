5a0f0423b0ff961b8ee9614e1d1c72e138810a2c
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-07 01:04:17 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://hungry-kings-laugh.loca.lt/api",
-    AUTH_BASE_URL: "https://hungry-kings-laugh.loca.lt/auth"
+    API_BASE_URL: "https://many-tools-accept.loca.lt/api",
+    AUTH_BASE_URL: "https://many-tools-accept.loca.lt/auth"
 }
