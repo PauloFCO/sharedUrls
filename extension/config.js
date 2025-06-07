4d62312c9240d2384f9d7c87f4d508f9419eb7da
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-07 03:05:00 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://many-tools-accept.loca.lt/api",
-    AUTH_BASE_URL: "https://many-tools-accept.loca.lt/auth"
+    API_BASE_URL: "https://brown-cows-agree.loca.lt/api",
+    AUTH_BASE_URL: "https://brown-cows-agree.loca.lt/auth"
 }
