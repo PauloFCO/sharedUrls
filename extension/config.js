cf1b958c92362cc847ddad0c7721e3150d9112e3
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-08 01:12:51 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://heavy-jars-help.loca.lt/api",
-    AUTH_BASE_URL: "https://heavy-jars-help.loca.lt/auth"
+    API_BASE_URL: "https://long-feet-attack.loca.lt/api",
+    AUTH_BASE_URL: "https://long-feet-attack.loca.lt/auth"
 }
