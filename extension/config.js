50a6b6013d4360966984ab0473c452bdf95f2c85
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-08 17:18:34 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://full-shoes-try.loca.lt/api",
-    AUTH_BASE_URL: "https://full-shoes-try.loca.lt/auth"
+    API_BASE_URL: "https://hip-mice-switch.loca.lt/api",
+    AUTH_BASE_URL: "https://hip-mice-switch.loca.lt/auth"
 }
