610b75b09fb17fd78e028d2b016d14092db3f8d8
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-08 03:13:34 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://long-feet-attack.loca.lt/api",
-    AUTH_BASE_URL: "https://long-feet-attack.loca.lt/auth"
+    API_BASE_URL: "https://seven-hats-dream.loca.lt/api",
+    AUTH_BASE_URL: "https://seven-hats-dream.loca.lt/auth"
 }
