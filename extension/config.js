6529f8cf9f21f8494452ba6df2af31e18cbd34d0
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-08 21:20:00 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://beige-chairs-nail.loca.lt/api",
-    AUTH_BASE_URL: "https://beige-chairs-nail.loca.lt/auth"
+    API_BASE_URL: "https://rich-parks-fail.loca.lt/api",
+    AUTH_BASE_URL: "https://rich-parks-fail.loca.lt/auth"
 }
