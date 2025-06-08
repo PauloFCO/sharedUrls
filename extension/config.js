df2a141e93ad6aec9d48766726b5bc09d82bc2aa
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-08 19:19:17 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://hip-mice-switch.loca.lt/api",
-    AUTH_BASE_URL: "https://hip-mice-switch.loca.lt/auth"
+    API_BASE_URL: "https://beige-chairs-nail.loca.lt/api",
+    AUTH_BASE_URL: "https://beige-chairs-nail.loca.lt/auth"
 }
