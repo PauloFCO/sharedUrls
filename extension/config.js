152bf2cf09a3be6f53e67c03dccc3db9e0565339
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-08 15:17:52 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://pink-lands-sink.loca.lt/api",
-    AUTH_BASE_URL: "https://pink-lands-sink.loca.lt/auth"
+    API_BASE_URL: "https://full-shoes-try.loca.lt/api",
+    AUTH_BASE_URL: "https://full-shoes-try.loca.lt/auth"
 }
