ed4a19df39cff1d83b4740372b0dc8e7d52a9cc5
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-08 05:14:17 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://seven-hats-dream.loca.lt/api",
-    AUTH_BASE_URL: "https://seven-hats-dream.loca.lt/auth"
+    API_BASE_URL: "https://polite-jokes-boil.loca.lt/api",
+    AUTH_BASE_URL: "https://polite-jokes-boil.loca.lt/auth"
 }
