721b6b96d96ddeeab4506384e59965dfaf99a11e
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-09 13:25:43 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://sad-ears-beam.loca.lt/api",
-    AUTH_BASE_URL: "https://sad-ears-beam.loca.lt/auth"
+    API_BASE_URL: "https://violet-rabbits-hunt.loca.lt/api",
+    AUTH_BASE_URL: "https://violet-rabbits-hunt.loca.lt/auth"
 }
