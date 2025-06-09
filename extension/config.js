22327d1317c45418dce5fefa0278798765fceea2
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-09 01:21:26 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://green-bars-hang.loca.lt/api",
-    AUTH_BASE_URL: "https://green-bars-hang.loca.lt/auth"
+    API_BASE_URL: "https://silly-insects-greet.loca.lt/api",
+    AUTH_BASE_URL: "https://silly-insects-greet.loca.lt/auth"
 }
