2007d65b734e5590d9f0d07db39b1dda757cfda7
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-09 23:29:19 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://fair-owls-camp.loca.lt/api",
-    AUTH_BASE_URL: "https://fair-owls-camp.loca.lt/auth"
+    API_BASE_URL: "https://spotty-days-turn.loca.lt/api",
+    AUTH_BASE_URL: "https://spotty-days-turn.loca.lt/auth"
 }
