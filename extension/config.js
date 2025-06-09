440745d5fe33bdc83cf2bd7c9b23125edabf8bed
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-09 09:24:17 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://ready-bananas-repair.loca.lt/api",
-    AUTH_BASE_URL: "https://ready-bananas-repair.loca.lt/auth"
+    API_BASE_URL: "https://bumpy-cats-kneel.loca.lt/api",
+    AUTH_BASE_URL: "https://bumpy-cats-kneel.loca.lt/auth"
 }
