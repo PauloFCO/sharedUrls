78feee507d11524cf6cface1d502f827d914dd26
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-09 19:27:53 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://brave-impalas-wear.loca.lt/api",
-    AUTH_BASE_URL: "https://brave-impalas-wear.loca.lt/auth"
+    API_BASE_URL: "https://lovely-years-stare.loca.lt/api",
+    AUTH_BASE_URL: "https://lovely-years-stare.loca.lt/auth"
 }
