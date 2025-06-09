fcc575b15335354ec3eed4758538e11ca362cf28
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-09 07:23:34 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://petite-humans-wave.loca.lt/api",
-    AUTH_BASE_URL: "https://petite-humans-wave.loca.lt/auth"
+    API_BASE_URL: "https://ready-bananas-repair.loca.lt/api",
+    AUTH_BASE_URL: "https://ready-bananas-repair.loca.lt/auth"
 }
