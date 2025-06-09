8497741237c475b11439babe66144726f9d4196a
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-09 11:25:00 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://bumpy-cats-kneel.loca.lt/api",
-    AUTH_BASE_URL: "https://bumpy-cats-kneel.loca.lt/auth"
+    API_BASE_URL: "https://sad-ears-beam.loca.lt/api",
+    AUTH_BASE_URL: "https://sad-ears-beam.loca.lt/auth"
 }
