665ea5ca63c42f689f319200fd008298c10e0f46
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-10 01:30:02 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://spotty-days-turn.loca.lt/api",
-    AUTH_BASE_URL: "https://spotty-days-turn.loca.lt/auth"
+    API_BASE_URL: "https://purple-crews-care.loca.lt/api",
+    AUTH_BASE_URL: "https://purple-crews-care.loca.lt/auth"
 }
