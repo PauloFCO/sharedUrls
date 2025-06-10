27e69ad23048e29f0c36ad89058e3dc405e2847e
PauloFCO <pauloqueteiglesias@gmail.com>
2025-06-10 07:32:11 +0000

    Actualización de las URLs de la aplicación automática y extensión

--- extension/config.js
+++ extension/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: "https://tricky-chicken-doubt.loca.lt/api",
-    AUTH_BASE_URL: "https://tricky-chicken-doubt.loca.lt/auth"
+    API_BASE_URL: "https://wild-mugs-flash.loca.lt/api",
+    AUTH_BASE_URL: "https://wild-mugs-flash.loca.lt/auth"
 }
